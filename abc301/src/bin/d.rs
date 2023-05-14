bfcbb446befe7210ba2e1eb1e5af8cc524024aa4
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-05-14 17:17:05 +0900

    upsolve

--- abc301/src/bin/d.rs
+++ abc301/src/bin/d.rs
@@ -1,7 +1,26 @@
+use proconio::marker::Bytes;
 use proconio::*;
 
 fn main() {
     input! {
-        
+        s: Bytes,
+        n: i64,
     }
+    let mut ans = 0i64;
+    for i in 0..s.len() {
+        ans <<= 1;
+        if s[i] == b'1' {
+            ans |= 1;
+        }
+    }
+    for i in 0..s.len() {
+        let x = 1 << (s.len() - (i + 1));
+        if s[i] == b'?' && ans + x <= n {
+            ans |= x;
+        }
+    }
+    if ans > n {
+        ans = -1;
+    }
+    println!("{}", ans);
 }
