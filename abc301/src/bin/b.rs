d40b8c692fff5d788ac52fea9f661a2eb2915a67
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-05-14 15:49:09 +0900

    imitate other's code

--- abc301/src/bin/b.rs
+++ abc301/src/bin/b.rs
@@ -1,21 +1,23 @@
 use proconio::*;
+use itertools::Itertools;
 
 fn main() {
     input! {
         n: usize,
         a: [i32; n],
     }
-    for i in 0..n - 1 {
-        print!("{} ", a[i]);
-        if a[i] < a[i + 1] {
-            for x in a[i] + 1..a[i + 1] {
-                print!("{} ", x);
+    let mut res = vec![a[0]];
+    for i in 1..n {
+        if a[i - 1] < a[i] {
+            for x in a[i - 1] + 1..a[i] {
+                res.push(x);
             }
         } else {
-            for x in (a[i + 1] + 1..a[i]).rev() {
-                print!("{} ", x);
+            for x in (a[i] + 1..a[i - 1]).rev() {
+                res.push(x);
             }
         }
+        res.push(a[i]);
     }
-    println!("{}", a[n - 1]);
+    println!("{}", res.iter().join(" "));
 }
