8a1a0a0c06f623df405b0928299c828a5a454848
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-05-25 19:18:58 +0900

    in contest agc062_a

--- agc062/src/bin/a.rs
+++ agc062/src/bin/a.rs
@@ -1,7 +1,34 @@
+use proconio::marker::Chars;
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
-    input! {}
-    unimplemented!();
+    input! {
+        t: usize,
+    }
+    for _ in 0..t {
+        input! {
+            _n: usize,
+            s: Chars,
+        }
+        println!("{}", solve(s));
+    }
+}
+
+fn solve(s: Vec<char>) -> char {
+    let n = s.len();
+    let mut b_exists = false;
+    let mut ba_exists = false;
+    for i in 1..n {
+        if s[i - 1] == 'B' && s[i] == 'A' {
+            ba_exists = true;
+        }
+        if s[i] == 'B' {
+            b_exists = true;
+        }
+    }
+    if !b_exists || ba_exists {
+        'A'
+    } else {
+        'B'
+    }
 }
