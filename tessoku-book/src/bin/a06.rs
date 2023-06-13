74aa69ef8c0eba7fd2d21c58401928482c0263a5
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-06-13 22:22:55 +0900

    shojin

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,9 +1,16 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize, q: usize,
+        a: [i32; n],
+        lr: [(usize, usize); q],
     }
-    unimplemented!();
+    let mut sum = vec![0; n + 1];
+    for i in 0..n {
+        sum[i + 1] = sum[i] + a[i];
+    }
+    for (l, r) in lr {
+        println!("{}", sum[r] - sum[l - 1]);
+    }
 }
