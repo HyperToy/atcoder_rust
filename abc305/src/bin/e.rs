318bb00fbd278b0c54a0e439c61a7303b37af48b
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-06-13 22:16:27 +0900

    file generate

---
+++ abc305/src/bin/e.rs
@@ -0,0 +1,10 @@
+use proconio::*;
+// use proconio::marker::Chars;
+// use proconio::marker::Bytes;
+// use itertools::Itertools;
+fn main() {
+    input! {
+        //
+    }
+    unimplemented!();
+}
