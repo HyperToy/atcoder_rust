5099b46f4260252b2fac54bafcb7d61af6eab65d
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-06-14 22:56:27 +0900

    shojin

--- tessoku-book/src/bin/a08.rs
+++ tessoku-book/src/bin/a08.rs
@@ -1,9 +1,32 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        h: usize, w: usize,
+        x: [[i32; w]; h],
+        q: usize,
+        ps: [(usize, usize, usize, usize); q],
     }
-    todo!();
+    let mut sum = vec![vec![0; w + 1]; h + 1];
+    for i in 0..h {
+        for j in 0..w {
+            sum[i + 1][j + 1] = x[i][j];
+        }
+    }
+    for i in 0..=h {
+        for j in 1..=w {
+            sum[i][j] += sum[i][j - 1];
+        }
+    }
+    for j in 0..=w {
+        for i in 1..=h {
+            sum[i][j] += sum[i - 1][j];
+        }
+    }
+    for (a, b, c, d) in ps {
+        println!(
+            "{}",
+            sum[c][d] - sum[c][b - 1] - sum[a - 1][d] + sum[a - 1][b - 1]
+        );
+    }
 }
