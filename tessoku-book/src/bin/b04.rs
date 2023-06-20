6f6c692180319fc4088aa73af56a9905a479309b
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-06-21 08:46:09 +0900

    refactor

--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,15 +1,8 @@
-use proconio::{marker::Chars, *};
+use proconio::*;
 
 fn main() {
     input! {
-        n: Chars,
+        n: String,
     }
-    let mut answer = 0;
-    for c in n {
-        answer <<= 1;
-        if c == '1' {
-            answer |= 1;
-        }
-    }
-    println!("{}", answer);
+    println!("{}", i32::from_str_radix(n.as_str(), 2).unwrap());
 }
