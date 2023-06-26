06d052a9cff300015d2e39bcc66249930f51983c
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-06-26 21:27:08 +0900

    shojin

--- tessoku-book/src/bin/b11.rs
+++ tessoku-book/src/bin/b11.rs
@@ -1,9 +1,25 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: isize,
+        mut a: [i32; n],
+        q: isize,
+        xs: [i32; q],
     }
-    todo!();
+    a.sort();
+    for x in xs {
+        // a[i] < x を満たす最大の i を求める
+        let mut ok = -1;
+        let mut ng = n;
+        while ng - ok > 1 {
+            let wj = (ok + ng) / 2;
+            if a[wj as usize] < x {
+                ok = wj;
+            } else {
+                ng = wj;
+            }
+        }
+        println!("{}", ok + 1);
+    }
 }
