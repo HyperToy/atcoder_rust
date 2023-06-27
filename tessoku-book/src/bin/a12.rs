e3d2c1fdbd84ceab3a767453f68778393b65ebb4
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-06-27 12:56:24 +0900

    shojin

--- tessoku-book/src/bin/a12.rs
+++ tessoku-book/src/bin/a12.rs
@@ -1,9 +1,32 @@
+/**
+ * 答えで二分探索
+ * 題意を十分に満たす X のうち、最小のものが求めるべき答え。
+ * すなわち、 X は題意を満たすが(ok)、X - 1 は題意を満たさない(ng)。
+ * ok = ∞ のとき、題意を満たす。 ng = 0 のとき、題意を満たさない。
+ */
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize, k: i64,
+        a: [i64; n],
     }
-    todo!();
+    let check = |wj| {
+        let mut sum = 0;
+        for x in &a {
+            sum += wj / x;
+        }
+        sum >= k
+    };
+    let mut ng = 0;
+    let mut ok = 1_000_000_000;
+    while ok - ng > 1 {
+        let wj = (ok + ng) / 2;
+        if check(wj) {
+            ok = wj;
+        } else {
+            ng = wj;
+        }
+    }
+    println!("{}", ok);
 }
--- tessoku-book/src/bin/b12.rs
+++ tessoku-book/src/bin/b12.rs
@@ -1,9 +1,19 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: f64,
     }
-    todo!();
+    let f = |x: f64| x.powf(3.) + x;
+    let mut ng = 0.;
+    let mut ok = 100.;
+    while (f(ok) - n).abs() > 1e-6 {
+        let wj = (ng + ok) / 2.;
+        if f(wj) > n {
+            ok = wj;
+        } else {
+            ng = wj;
+        }
+    }
+    println!("{:.6}", ok);
 }
