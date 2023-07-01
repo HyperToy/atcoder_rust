9226e0b3cb7e5aca610724af9aea84baa52adebf
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-01 10:20:30 +0900

    shojin

--- tessoku-book/src/bin/a15.rs
+++ tessoku-book/src/bin/a15.rs
@@ -1,9 +1,34 @@
+use itertools::Itertools;
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i32; n],
     }
-    todo!();
+
+    let mut v = a.clone();
+    v.sort();
+    v.dedup();
+
+    let get_order = |x| {
+        let mut ok = 0;
+        let mut ng = v.len();
+        while ng - ok > 1 {
+            let wj = (ok + ng) / 2;
+            if v[wj] <= x {
+                ok = wj;
+            } else {
+                ng = wj;
+            }
+        }
+        ok + 1
+    };
+
+    let mut compressed = Vec::new();
+    for i in 0..n {
+        compressed.push(get_order(a[i]));
+    }
+
+    println!("{}", compressed.iter().join(" "));
 }
