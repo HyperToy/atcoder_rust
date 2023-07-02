cd76b05c874306bf150d6537df1ca4d0751a88d2
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-02 22:31:24 +0900

    shojin

--- tessoku-book/src/bin/a16.rs
+++ tessoku-book/src/bin/a16.rs
@@ -1,9 +1,20 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i32; n - 1],
+        b: [i32; n - 2],
     }
-    todo!();
+    let mut dp = vec![std::i32::MAX; n];
+    dp[0] = 0;
+    for i in 0..n {
+        if i >= 1 {
+            dp[i] = dp[i].min(dp[i - 1] + a[i - 1]);
+        }
+        if i >= 2 {
+            dp[i] = dp[i].min(dp[i - 2] + b[i - 2]);
+        }
+    }
+    println!("{}", dp[n - 1]);
 }
