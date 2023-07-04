e3669490db04286f917b95f52f8f5d82b0b9bf40
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-04 11:40:25 +0900

    shojin: dp restore

--- tessoku-book/src/bin/a18.rs
+++ tessoku-book/src/bin/a18.rs
@@ -1,9 +1,22 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize, s: usize,
+        a: [usize; n],
     }
-    todo!();
+    // dp[i][j] := i 番目までで 合計を j にする方法はあるか
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+    for i in 0..n {
+        for j in 0..=s {
+            if dp[i][j] {
+                dp[i + 1][j] = true;
+                if j + a[i] <= s {
+                    dp[i + 1][j + a[i]] = true;
+                }
+            }
+        }
+    }
+    println!("{}", if dp[n][s] { "Yes" } else { "No" });
 }
--- tessoku-book/src/bin/b18.rs
+++ tessoku-book/src/bin/b18.rs
@@ -1,9 +1,37 @@
+use itertools::Itertools;
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize, s: usize,
+        a: [usize; n],
     }
-    todo!();
+    // dp[i][j] := i 番目までで 合計を j にする方法はあるか
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+    dp[0][0] = true;
+    for i in 0..n {
+        for j in 0..=s {
+            if dp[i][j] {
+                dp[i + 1][j] = true;
+                if j + a[i] <= s {
+                    dp[i + 1][j + a[i]] = true;
+                }
+            }
+        }
+    }
+    if !dp[n][s] {
+        println!("{}", -1);
+        return;
+    }
+    let mut answer = Vec::new();
+    let mut number = s;
+    for i in (0..n).rev() {
+        if number >= a[i] && dp[i][number - a[i]] {
+            answer.push(i);
+            number -= a[i];
+        }
+    }
+    answer.reverse();
+    println!("{}", answer.len());
+    println!("{}", answer.iter().map(|x| x + 1).join(" "));
 }
