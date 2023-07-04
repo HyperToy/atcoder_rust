248d15b07cd8fa2ea02be6fc4fbb573ff25adea5
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-05 07:34:00 +0900

    shojin

--- tessoku-book/src/bin/a19.rs
+++ tessoku-book/src/bin/a19.rs
@@ -1,9 +1,18 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize, w: usize,
+        items: [(usize, usize); n],
     }
-    todo!();
+    let mut dp = vec![vec![0; w + 1]; n + 1];
+    for i in 0..n {
+        for j in 0..=w {
+            dp[i + 1][j] = dp[i + 1][j].max(dp[i][j]);
+            if j + items[i].0 <= w {
+                dp[i + 1][j + items[i].0] = dp[i + 1][j + items[i].0].max(dp[i][j] + items[i].1);
+            }
+        }
+    }
+    println!("{}", dp[n][w]);
 }
