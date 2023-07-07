7a65a340715f5a56f3d9cb606a2ed9c7719bcd69
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-07 22:02:18 +0900

    shojin

--- tessoku-book/src/bin/a24.rs
+++ tessoku-book/src/bin/a24.rs
@@ -1,9 +1,73 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [usize; n],
     }
-    todo!();
+    println!("{}", longest_increasing_subsequence(&a));
+}
+
+fn longest_increasing_subsequence(a: &Vec<usize>) -> usize {
+    let n = a.len();
+    let mut lis = vec![1_000_000; n];
+    for i in 0..n {
+        let pos = lis.lower_bound(&a[i]);
+        lis[pos] = a[i];
+    }
+    lis.lower_bound(&1_000_000)
+}
+
+use std::cmp::Ordering;
+
+/// Equivalent to std::lowerbound and std::upperbound in c++
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &T) -> usize;
+    fn upper_bound(&self, x: &T) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [T] {
+    fn lower_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less => {
+                    low = mid + 1;
+                }
+                Ordering::Equal | Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+
+    fn upper_bound(&self, x: &T) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            match self[mid].cmp(x) {
+                Ordering::Less | Ordering::Equal => {
+                    low = mid + 1;
+                }
+                Ordering::Greater => {
+                    high = mid;
+                }
+            }
+        }
+        low
+    }
+}
+
+#[test]
+fn test_binary_search() {
+    let vec = vec![1, 2, 4, 6, 7, 12, 54, 60];
+
+    assert_eq!(vec.lower_bound(&4), 2);
+    assert_eq!(vec.upper_bound(&4), 3);
 }
