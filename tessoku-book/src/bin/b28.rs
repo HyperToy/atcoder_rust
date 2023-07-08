01790cf09d9e4cc9e526844d75bdf5ae53ad7d7e
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-09 08:57:26 +0900

    shojin

--- tessoku-book/src/bin/b28.rs
+++ tessoku-book/src/bin/b28.rs
@@ -1,9 +1,18 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
     }
-    todo!();
+    let a = fibonacci(n, 1000000000 + 7);
+    println!("{}", a[n]);
+}
+fn fibonacci(max: usize, modulo: u64) -> Vec<u64> {
+    let mut res = vec![0; max + 1];
+    res[1] = 1;
+    res[2] = 1;
+    for i in 3..=max {
+        res[i] = (res[i - 1] + res[i - 2]) % modulo;
+    }
+    res
 }
