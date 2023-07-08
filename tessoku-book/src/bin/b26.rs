57e41fd6977dff2317ce38b9ac82a748070111e1
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-08 22:33:48 +0900

    shojin

--- tessoku-book/src/bin/b26.rs
+++ tessoku-book/src/bin/b26.rs
@@ -1,9 +1,35 @@
+use itertools::Itertools;
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
     }
-    todo!();
+    let prime_numbers = make_prime_numbers(n);
+    println!("{}", prime_numbers.iter().join(" "));
+}
+fn make_prime_numbers(max: usize) -> Vec<usize> {
+    let mut deleted = vec![false; max + 1];
+    deleted[0] = true;
+    deleted[1] = true;
+    let mut i = 2;
+    while i * i <= max {
+        if deleted[i] {
+            i += 1;
+            continue;
+        }
+        let mut j = i * 2;
+        while j <= max {
+            deleted[j] = true;
+            j += i;
+        }
+        i += 1;
+    }
+    let mut res = Vec::new();
+    for i in 0..=max {
+        if !deleted[i] {
+            res.push(i);
+        }
+    }
+    res
 }
