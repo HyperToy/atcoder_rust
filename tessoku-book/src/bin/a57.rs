a84b32e17639e2747f975751e67086b7db4c11ec
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-15 17:31:31 +0900

    shojin

--- tessoku-book/src/bin/a57.rs
+++ tessoku-book/src/bin/a57.rs
@@ -1,9 +1,28 @@
-use proconio::*;
-// use itertools::Itertools;
+use proconio::{marker::Usize1, *};
 
 fn main() {
     input! {
-        //
+        n: usize, q: usize,
+        a: [Usize1; n],
+        qs: [(Usize1, usize); q],
     }
-    todo!();
+    // dp[i][j] := 位置 i にいた 2^j 日後にいる位置
+    let mut dp = vec![vec![0; 30]; n];
+    for i in 0..n {
+        dp[i][0] = a[i];
+    }
+    for j in 1..30 {
+        for i in 0..n {
+            dp[i][j] = dp[dp[i][j - 1]][j - 1];
+        }
+    }
+    for (x, y) in qs {
+        let mut pos = x;
+        for i in 0..30 {
+            if (y >> i & 1) == 1 {
+                pos = dp[pos][i];
+            }
+        }
+        println!("{}", pos + 1);
+    }
 }
--- tessoku-book/src/bin/b57.rs
+++ tessoku-book/src/bin/b57.rs
@@ -1,9 +1,36 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize, k: usize,
     }
-    todo!();
+    // dp[i][j] := i に 2^j 回操作を行ったときの値
+    let mut dp = vec![vec![0; 40]; n + 1];
+    for i in 0..=n {
+        dp[i][0] = operate(i);
+    }
+    for j in 1..40 {
+        for i in 0..=n {
+            dp[i][j] = dp[dp[i][j - 1]][j - 1];
+        }
+    }
+
+    for i in 1..=n {
+        let mut result = i;
+        for j in 0..40 {
+            if (k >> j & 1) == 1 {
+                result = dp[result][j];
+            }
+        }
+        println!("{}", result);
+    }
+}
+
+fn operate(mut x: usize) -> usize {
+    let mut result = x;
+    while x > 0 {
+        result -= x % 10;
+        x /= 10;
+    }
+    result
 }
