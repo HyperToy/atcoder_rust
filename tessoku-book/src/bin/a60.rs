209068b4f0f80cc1fcf3890a3eb61729812946cd
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-19 09:38:05 +0900

    shojin

--- tessoku-book/src/bin/a60.rs
+++ tessoku-book/src/bin/a60.rs
@@ -1,9 +1,20 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [usize; n],
     }
-    todo!();
+    let mut stack: Vec<(usize, usize)> = Vec::new();
+    for i in 0..n {
+        if stack.is_empty() {
+            println!("{}", -1);
+        } else {
+            while stack.last().unwrap().1 < a[i] {
+                stack.pop();
+            }
+            println!("{}", stack.last().unwrap().0);
+        }
+        stack.push((i + 1, a[i]));
+    }
 }
--- tessoku-book/src/bin/a61.rs
+++ tessoku-book/src/bin/a61.rs
@@ -1,9 +1,25 @@
-use proconio::*;
-// use itertools::Itertools;
+use proconio::{marker::Usize1, *};
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        vs: [(Usize1, Usize1); m],
     }
-    todo!();
+    let mut g = vec![Vec::new(); n];
+    for (u, v) in vs {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    for u in 0..n {
+        print!("{}: {}", u + 1, '{');
+        g[u].sort();
+        for v in 0..g[u].len() {
+            print!(
+                "{}{}",
+                g[u][v] + 1,
+                if v == g[u].len() - 1 { "" } else { ", " }
+            );
+        }
+        println!("{}", '}');
+    }
 }
