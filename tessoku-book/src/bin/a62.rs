65102b1df02c1fca0125a5210edf62588e5b5937
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-23 11:09:04 +0900

    shojin

--- tessoku-book/src/bin/a62.rs
+++ tessoku-book/src/bin/a62.rs
@@ -1,9 +1,36 @@
-use proconio::*;
-// use itertools::Itertools;
+use proconio::{marker::Usize1, *};
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        es: [(Usize1, Usize1); m],
     }
-    todo!();
+    let mut g = vec![Vec::new(); n];
+    for (u, v) in es {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let mut stack = Vec::new();
+    let mut seen = vec![false; n];
+    let start = 0;
+    stack.push(start);
+    seen[start] = true;
+    while !stack.is_empty() {
+        let u = stack.pop().unwrap();
+        // refactor .clone()
+        for v in g[u].clone() {
+            if !seen[v] {
+                stack.push(v);
+                seen[v] = true;
+            }
+        }
+    }
+    println!(
+        "The graph {} connected.",
+        if seen.iter().all(|x| *x) {
+            "is"
+        } else {
+            "is not"
+        }
+    );
 }
--- tessoku-book/src/bin/a63.rs
+++ tessoku-book/src/bin/a63.rs
@@ -1,9 +1,32 @@
-use proconio::*;
-// use itertools::Itertools;
+use proconio::{marker::Usize1, *};
+use std::collections::VecDeque;
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        es: [(Usize1, Usize1); m],
     }
-    todo!();
+    let mut g = vec![Vec::new(); n];
+    for (u, v) in es {
+        g[u].push(v);
+        g[v].push(u);
+    }
+    let mut dist = vec![std::i32::MAX; n];
+    let mut queue = VecDeque::new();
+    let start = 0;
+    dist[start] = 0;
+    queue.push_back(start);
+    while !queue.is_empty() {
+        let u = queue.pop_front().unwrap();
+        // refactor .clone()
+        for v in g[u].clone() {
+            if dist[v] > dist[u] {
+                dist[v] = dist[u] + 1;
+                queue.push_back(v);
+            }
+        }
+    }
+    for i in 0..n {
+        println!("{}", if dist[i] < std::i32::MAX { dist[i] } else { -1 });
+    }
 }
