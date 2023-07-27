fff2b582dccc07fc16e2c1e2061329e655986b93
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-07-28 01:22:19 +0900

    shojin

--- tessoku-book/src/bin/a77.rs
+++ tessoku-book/src/bin/a77.rs
@@ -1,5 +1,4 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
@@ -12,11 +11,23 @@ fn main() {
     let mut ng = l + 1;
     while ok + 1 < ng {
         let wj = (ok + ng) / 2;
-        if check(wj) {
+        if check(wj, &a) >= k + 1 {
             ok = wj;
         } else {
             ng = wj;
         }
     }
     println!("{}", ok);
+}
+
+fn check(x: usize, a: &Vec<usize>) -> usize {
+    let mut result = 0;
+    let mut pos = 0;
+    for i in 0..a.len() {
+        if a[i] - pos >= x {
+            result += 1;
+            pos = a[i];
+        }
+    }
+    result
 }
