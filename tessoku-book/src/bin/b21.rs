fff5853843d4dbc462d5bf9d05592b8ea83579e5
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-08-22 09:33:10 +0900

    shojin

--- tessoku-book/src/bin/b21.rs
+++ tessoku-book/src/bin/b21.rs
@@ -1,9 +1,22 @@
-use proconio::*;
-// use itertools::Itertools;
+use proconio::{marker::Chars, *};
 
 fn main() {
     input! {
-        //
+        n: usize,
+        s: Chars,
     }
-    todo!();
+    let mut dp = vec![vec![0; n]; n];
+    for i in 0..n {
+        dp[i][i] = 1;
+    }
+    for r in 0..n {
+        for l in (0..r).rev() {
+            dp[l][r] = if s[l] == s[r] {
+                2 + dp[l + 1][r - 1]
+            } else {
+                std::cmp::max(dp[l][r - 1], dp[l + 1][r])
+            };
+        }
+    }
+    println!("{}", dp[0][n - 1])
 }
