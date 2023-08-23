bcd9065dca52db60e36f0f35424396ad2d96d7f9
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-08-23 13:37:09 +0900

    shojin

--- tessoku-book/src/bin/b23.rs
+++ tessoku-book/src/bin/b23.rs
@@ -1,9 +1,35 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        cities: [(f64, f64); n],
     }
-    todo!();
+    let mut dist = vec![vec![0.; n]; n];
+    for i in 0..n {
+        for j in 0..n {
+            dist[i][j] = calc_distance(cities[i], cities[j])
+        }
+    }
+    let mut dp = vec![vec![1_000_000_000.; n]; 1 << n];
+    dp[0][0] = 0.;
+    for s in 0..(1 << n) {
+        for v in 0..n {
+            for u in 0..n {
+                if s != 0 && (s & (1 << u)) == 0 {
+                    continue;
+                }
+                if (s & (1 << v)) == 0 && u != v {
+                    if dp[s | (1 << v)][v] > dp[s][u] + dist[u][v] {
+                        dp[s | (1 << v)][v] = dp[s][u] + dist[u][v];
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", dp[(1 << n) - 1][0]);
+}
+
+fn calc_distance(a: (f64, f64), b: (f64, f64)) -> f64 {
+    ((a.0 - b.0).powf(2.) + (a.1 - b.1).powf(2.)).sqrt()
 }
--- tessoku-book/src/bin/b24.rs
+++ tessoku-book/src/bin/b24.rs
@@ -1,9 +1,58 @@
 use proconio::*;
-// use itertools::Itertools;
+use std::cmp::Ordering;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        mut boxes: [(usize, usize); n],
     }
-    todo!();
+    boxes.sort_by(|a, b| match a.0.cmp(&b.0) {
+        Ordering::Greater | Ordering::Less => a.0.cmp(&b.0),
+        Ordering::Equal => b.1.cmp(&a.1),
+    });
+    println!("{}", longest_increasing_subsequence(&boxes));
+}
+
+fn longest_increasing_subsequence<T: Ord + Copy>(a: &Vec<(T, T)>) -> usize {
+    let n = a.len();
+    let mut lis = Vec::new();
+    for i in 0..n {
+        let pos = lis.lower_bound(&a[i]);
+        if pos < lis.len() {
+            lis[pos] = a[i];
+        } else {
+            lis.push(a[i]);
+        }
+    }
+    lis.len()
+}
+
+/// Equivalent to std::lower_bound and std::upper_bound in c++
+pub trait BinarySearch<T> {
+    fn lower_bound(&self, x: &(T, T)) -> usize;
+}
+
+impl<T: Ord> BinarySearch<T> for [(T, T)] {
+    fn lower_bound(&self, x: &(T, T)) -> usize {
+        let mut low = 0;
+        let mut high = self.len();
+
+        while low != high {
+            let mid = (low + high) / 2;
+            // match self[mid].cmp(x) {
+            //     Ordering::Less => {
+            //         low = mid + 1;
+            //     }
+            //     Ordering::Equal | Ordering::Greater => {
+            //         high = mid;
+            //     }
+            // }
+            if self[mid].0 < x.0 && self[mid].1 < x.1 {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        low
+    }
 }
