7eb1313ab55963b1235f47a540ae9d48e5c17e36
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-09-05 20:59:42 +0900

    shojin

--- tessoku-book/src/bin/b59.rs
+++ tessoku-book/src/bin/b59.rs
@@ -1,9 +1,54 @@
-use proconio::*;
-// use itertools::Itertools;
+use proconio::{marker::Usize1, *};
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [Usize1; n],
     }
-    todo!();
+    let mut answer = 0;
+    let mut rsq = SegTree::new(n, 0);
+    for j in 0..n {
+        answer += rsq.query(a[j] + 1, n);
+        rsq.add(a[j], 1);
+    }
+    println!("{}", answer);
+}
+
+struct SegTree {
+    data: Vec<usize>,
+    size: usize,
+}
+impl SegTree {
+    fn new(n: usize, init_val: usize) -> Self {
+        let mut size = 1;
+        while size < n {
+            size *= 2;
+        }
+        let data = vec![init_val; size * 2];
+        Self { data, size }
+    }
+    fn add(&mut self, pos: usize, val: usize) {
+        let mut pos = pos + self.size;
+        self.data[pos] += val;
+        while pos > 1 {
+            pos /= 2;
+            self.data[pos] = self.data[pos * 2] + self.data[pos * 2 + 1];
+        }
+    }
+
+    fn query_sub(&self, l: usize, r: usize, a: usize, b: usize, k: usize) -> usize {
+        if r <= a || b <= l {
+            return 0;
+        }
+        if l <= a && b <= r {
+            return self.data[k];
+        }
+        let m = (a + b) / 2;
+        let val_l = self.query_sub(l, r, a, m, k * 2);
+        let val_r = self.query_sub(l, r, m, b, k * 2 + 1);
+        val_l + val_r
+    }
+    fn query(&self, l: usize, r: usize) -> usize {
+        self.query_sub(l, r, 0, self.size, 1)
+    }
 }
--- tessoku-book/src/bin/b64.rs
+++ tessoku-book/src/bin/b64.rs
@@ -1,9 +1,47 @@
-use proconio::*;
-// use itertools::Itertools;
+use itertools::Itertools;
+use proconio::{marker::Usize1, *};
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        es: [(Usize1, Usize1, i32); m],
     }
-    todo!();
+    let mut g = vec![Vec::new(); n];
+    for (u, v, w) in es {
+        g[u].push((v, w));
+        g[v].push((u, w));
+    }
+
+    // dijkstra
+    let mut dist = vec![std::i32::MAX; n];
+    let mut q = BinaryHeap::new();
+    let start = 0;
+    dist[start] = 0;
+    q.push((Reverse(dist[start]), start));
+
+    while !q.is_empty() {
+        let u = q.pop().unwrap().1;
+        for (v, w) in g[u].clone() {
+            if dist[v] > dist[u] + w {
+                dist[v] = dist[u] + w;
+                q.push((Reverse(dist[v]), v));
+            }
+        }
+    }
+
+    let mut answer = Vec::new();
+    let goal = n - 1;
+    let mut pos = goal;
+    answer.push(pos);
+    while pos != start {
+        for (v, w) in g[pos].clone() {
+            if dist[pos] - w == dist[v] {
+                answer.push(v);
+                pos = v;
+                break;
+            }
+        }
+    }
+    println!("{}", answer.iter().rev().map(|x| x + 1).join(" "));
 }
