eb1d45a3f19b47d6ca56db56ae2ed870ec973240
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-09-17 11:30:10 +0900

    shojin

--- tessoku-book/src/bin/c08.rs
+++ tessoku-book/src/bin/c08.rs
@@ -1,9 +1,42 @@
 use proconio::*;
-// use itertools::Itertools;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        st: [(i32, usize); n],
     }
-    todo!();
+    let mut answer = Vec::new();
+    for i in 0..10000 {
+        let mut ok = true;
+        for (s, t) in st.clone() {
+            let mut same_count = 0;
+            same_count += if s % 10 == i % 10 { 1 } else { 0 }; // 1の位
+            same_count += if s / 10 % 10 == i / 10 % 10 { 1 } else { 0 }; // 10の位
+            same_count += if s / 100 % 10 == i / 100 % 10 { 1 } else { 0 }; // 100の位
+            same_count += if s / 1000 % 10 == i / 1000 % 10 { 1 } else { 0 }; // 1000の位
+            match t {
+                1 => {
+                    ok &= same_count == 4;
+                }
+                2 => {
+                    ok &= same_count == 3;
+                }
+                3 => {
+                    ok &= same_count < 3;
+                }
+                _ => unreachable!(),
+            }
+        }
+        if ok {
+            answer.push(i);
+        }
+    }
+    println!(
+        "{}",
+        if answer.len() == 1 {
+            format!("{:04}", answer[0])
+        } else {
+            "Can't Solve".to_string()
+        }
+    );
 }
--- tessoku-book/src/bin/c11.rs
+++ tessoku-book/src/bin/c11.rs
@@ -1,9 +1,43 @@
+use itertools::Itertools;
 use proconio::*;
-// use itertools::Itertools;
+use std::collections::BinaryHeap;
 
+// TLE
 fn main() {
     input! {
-        //
+        n: usize, k: usize,
+        a: [i64; n],
     }
-    todo!();
+    let mut q = BinaryHeap::new();
+    for i in 0..n {
+        q.push((Rational::new(a[i], 1), i));
+    }
+    let mut answer = vec![0; n];
+    for _ in 0..k {
+        let v = q.pop().unwrap();
+        answer[v.1] += 1;
+        q.push((Rational::new(v.0.number, v.0.demon + 1), v.1));
+    }
+    println!("{}", answer.iter().join(" "));
+}
+
+#[derive(PartialEq, Eq)]
+struct Rational {
+    number: i64,
+    demon: i64,
+}
+impl PartialOrd for Rational {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        (self.number * other.demon).partial_cmp(&(other.number * self.demon))
+    }
+}
+impl Ord for Rational {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (self.number * other.demon).cmp(&(other.number * self.demon))
+    }
+}
+impl Rational {
+    fn new(number: i64, demon: i64) -> Self {
+        Self { number, demon }
+    }
 }
