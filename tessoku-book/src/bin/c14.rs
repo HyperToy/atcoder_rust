01cfaca684ca64395d7232bf0948961d1376da65
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-09-17 19:26:56 +0900

    shojin

--- tessoku-book/src/bin/c14.rs
+++ tessoku-book/src/bin/c14.rs
@@ -1,9 +1,55 @@
-use proconio::*;
-// use itertools::Itertools;
+use proconio::{marker::Usize1, *};
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, VecDeque},
+};
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        es: [(Usize1, Usize1, i32); m],
     }
-    todo!();
+    let mut g = vec![Vec::new(); n];
+    for (u, v, w) in es {
+        g[u].push((v, w));
+        g[v].push((u, w));
+    }
+
+    // dijkstra
+    let mut dist = vec![std::i32::MAX; n];
+    let mut q = BinaryHeap::new();
+    let start = 0;
+    dist[start] = 0;
+    q.push((Reverse(dist[start]), start));
+    while !q.is_empty() {
+        let u = q.pop().unwrap().1;
+        for (v, w) in g[u].clone() {
+            if dist[v] > dist[u] + w {
+                dist[v] = dist[u] + w;
+                q.push((Reverse(dist[v]), v));
+            }
+        }
+    }
+
+    // bfs
+    let mut seen = vec![false; n];
+    let mut q = VecDeque::new();
+    let goal = n - 1;
+    seen[goal] = true;
+    q.push_back(goal);
+    let mut answer = 1;
+    while !q.is_empty() {
+        let u = q.pop_front().unwrap();
+        for (v, w) in g[u].clone() {
+            if seen[v] {
+                continue;
+            }
+            if dist[v] == dist[u] - w {
+                q.push_back(v);
+                seen[v] = true;
+                answer += 1;
+            }
+        }
+    }
+    println!("{}", answer);
 }
