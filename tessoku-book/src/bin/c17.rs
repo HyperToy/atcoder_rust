8e8a1f1be76b53c08c0693a24049edf1ab8121aa
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-09-18 11:57:59 +0900

    shojin

--- tessoku-book/src/bin/c17.rs
+++ tessoku-book/src/bin/c17.rs
@@ -1,9 +1,47 @@
 use proconio::*;
-// use itertools::Itertools;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
-        //
+        q: usize,
     }
-    todo!();
+    let mut former = VecDeque::new();
+    let mut latter = VecDeque::new();
+
+    for _ in 0..q {
+        input! {
+            t: char,
+        }
+        match t {
+            'A' => {
+                input! {
+                    x: String,
+                }
+                latter.push_back(x);
+                if former.len() < latter.len() {
+                    former.push_back(latter.pop_front().unwrap());
+                }
+            }
+            'B' => {
+                input! {
+                    x: String,
+                }
+                if former.len() == latter.len() {
+                    former.push_back(x);
+                } else {
+                    latter.push_front(x);
+                }
+            }
+            'C' => {
+                former.pop_front();
+                if former.len() < latter.len() {
+                    former.push_back(latter.pop_front().unwrap());
+                }
+            }
+            'D' => {
+                println!("{}", former.front().unwrap());
+            }
+            _ => unreachable!(),
+        }
+    }
 }
