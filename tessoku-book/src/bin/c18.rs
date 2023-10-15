95738de078ab8e4832b556e6913d8388c9786eda
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-10-15 17:50:04 +0900

    shojin tessoku c18 refactor
    Option を使って「不可能手を表す十分大きな数」のハードコーディングを解消
    コードは増えたが実行は早くなった

--- tessoku-book/src/bin/c18.rs
+++ tessoku-book/src/bin/c18.rs
@@ -10,9 +10,9 @@ fn main() {
 
 fn solve(a: Vec<i32>) -> i32 {
     let n = a.len();
-    let mut dp = vec![vec![1_000_000_000; n + 1]; n];
+    let mut dp = vec![vec![None; n + 1]; n];
     for i in 0..n {
-        dp[i][i] = 0;
+        dp[i][i] = Some(0);
     }
     for len in 2..=n {
         for l in 0..n {
@@ -20,11 +20,36 @@ fn solve(a: Vec<i32>) -> i32 {
             if r > n {
                 break;
             }
-            dp[l][r] = dp[l][r].min(dp[l + 1][r - 1] + (a[l] - a[r - 1]).abs());
+            let mut now = if let Some(x) = dp[l + 1][r - 1] {
+                Some(x + (a[l] - a[r - 1]).abs())
+            } else {
+                None
+            };
             for m in l..r {
-                dp[l][r] = dp[l][r].min(dp[l][m] + dp[m][r]);
+                if let Some(x) = dp[l][m] {
+                    if let Some(y) = dp[m][r] {
+                        now = if let Some(v) = now {
+                            Some(v.min(x + y))
+                        } else {
+                            Some(x + y)
+                        }
+                    }
+                }
+            }
+            dp[l][r] = if let Some(x) = dp[l][r] {
+                if let Some(v) = now {
+                    Some(x.min(v))
+                } else {
+                    Some(x)
+                }
+            } else {
+                now
             }
         }
     }
-    dp[0][n]
+    if let Some(x) = dp[0][n] {
+        x
+    } else {
+        -1
+    }
 }
