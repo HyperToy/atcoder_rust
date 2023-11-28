6e0dd9b408fd34e7444d9f131c6ff3da87028a61
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-11-29 07:44:00 +0900

    refactor

--- abc329/src/bin/d.rs
+++ abc329/src/bin/d.rs
@@ -1,17 +1,21 @@
 use proconio::{marker::Usize1, *};
-use std::{cmp::Reverse, collections::BinaryHeap};
 
-// refactor
 fn main() {
     input! {
         n: usize, m: usize,
         a: [Usize1; m],
     }
-    let mut votes = BinaryHeap::new();
     let mut count = vec![0; n];
+    let mut winner = None;
     for x in a {
         count[x] += 1;
-        votes.push((count[x], Reverse(x)));
-        println!("{}", votes.peek().unwrap().1 .0 + 1);
+        if let Some(w) = winner {
+            if count[x] > count[w] || count[x] == count[w] && x < w {
+                winner = Some(x);
+            }
+        } else {
+            winner = Some(x);
+        }
+        println!("{}", winner.unwrap() + 1);
     }
 }
