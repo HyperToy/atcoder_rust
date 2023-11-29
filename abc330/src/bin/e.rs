2cf44dbf9b70a50436a48f7550e240e649f5c400
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-11-29 09:25:11 +0900

    refactor

--- abc330/src/bin/e.rs
+++ abc330/src/bin/e.rs
@@ -1,5 +1,5 @@
 use proconio::{input, marker::Usize1};
-use std::{cmp::Reverse, collections::BinaryHeap};
+use std::collections::BTreeSet;
 
 fn main() {
     input! {
@@ -8,7 +8,7 @@ fn main() {
         queries: [(Usize1, usize); q],
     }
     let mut count = vec![0; n + 1];
-    let mut zero = BinaryHeap::new();
+    let mut zero = BTreeSet::new();
     for i in 0..n {
         if a[i] <= n {
             count[a[i]] += 1;
@@ -16,29 +16,21 @@ fn main() {
     }
     for i in 0..=n {
         if count[i] == 0 {
-            zero.push(Reverse(i));
+            zero.insert(i);
         }
     }
     for (i, x) in queries {
         if a[i] <= n {
             count[a[i]] -= 1;
             if count[a[i]] == 0 {
-                zero.push(Reverse(a[i]));
+                zero.insert(a[i]);
             }
+        }
+        if x <= n {
+            count[x] += 1;
+            zero.remove(&x);
         }
         a[i] = x;
-        if a[i] <= n {
-            count[x] += 1;
-        }
-        loop {
-            let x: usize = zero.peek().unwrap().0;
-            if count[x] > 0 {
-                zero.pop();
-            } else {
-                println!("{}", x);
-                break;
-            }
-        }
-        // eprintln!("{:?}", zero);
+        println!("{}", zero.first().unwrap());
     }
 }
