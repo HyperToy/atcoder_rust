a4b590b8fc8a75fb63be1e5a1510f27efee94bc0
Ryohey <ryohey.trumpet+tech@gmail.com>
2023-11-29 18:54:03 +0900

    shojin

--- abc329/src/bin/f.rs
+++ abc329/src/bin/f.rs
@@ -1,8 +1,32 @@
-use proconio::*;
+use itertools::Itertools;
+use proconio::{marker::Usize1, *};
+use std::collections::HashSet;
 
 fn main() {
     input! {
-        //
+        n: usize, q: usize,
+        c: [i32; n],
+        queries: [(Usize1, Usize1); q],
     }
-    todo!();
+    let mut count = vec![HashSet::new(); n];
+    c.into_iter().enumerate().for_each(|(i, c)| {
+        count[i].insert(c);
+    });
+    let mut p = (0..n).collect::<Vec<_>>();
+    println!(
+        "{}",
+        queries
+            .into_iter()
+            .map(|(a, b)| {
+                if count[p[a]].len() > count[p[b]].len() {
+                    (p[a], p[b]) = (p[b], p[a]);
+                }
+                for c in count[p[a]].clone() {
+                    count[p[b]].insert(c);
+                }
+                count[p[a]].clear();
+                count[p[b]].len()
+            })
+            .join(" ")
+    );
 }
