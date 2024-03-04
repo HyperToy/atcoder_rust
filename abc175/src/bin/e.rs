8c6d0ff9856b4821795af409544a620e90ebd5e7
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-03-04 11:47:06 +0900

    refactor

--- abc175/src/bin/e.rs
+++ abc175/src/bin/e.rs
@@ -14,16 +14,20 @@ fn main() {
     // アイテムの価値の最大値
     let mut dp = vec![vec![[[std::i64::MIN; 2]; 4]; c]; r];
     dp[0][0][0][0] = 0;
+    // 配る DP
     for i in 0..r {
         for j in 0..c {
-            for k in 0..4 {
-                if field[i][j] != 0 && k < 3 {
+            for k in 0..=3 {
+                if field[i][j] > 0 && k < 3 {
+                    // アイテムを拾う
                     dp[i][j][k + 1][1] = dp[i][j][k + 1][1].max(dp[i][j][k][0] + field[i][j]);
                 }
                 if j < c - 1 {
+                    // 行の隣のマスに行く
                     dp[i][j + 1][k][0] = dp[i][j + 1][k][0].max(dp[i][j][k][0]).max(dp[i][j][k][1]);
                 }
                 if i < r - 1 {
+                    // 隣の列に行く
                     dp[i + 1][j][0][0] = dp[i + 1][j][0][0].max(dp[i][j][k][0]).max(dp[i][j][k][1]);
                 }
             }
