fda0ae108a46687cdc6d964f56b4267354473054
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-03-04 12:15:11 +0900

    shojin

--- abc138/src/bin/a.rs
+++ abc138/src/bin/a.rs
@@ -2,7 +2,8 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        a: i32,
+        s: String,
     }
-    todo!();
+    println!("{}", if a >= 3200 { s } else { "red".to_string() });
 }
--- abc138/src/bin/b.rs
+++ abc138/src/bin/b.rs
@@ -2,7 +2,9 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [f64; n],
     }
-    todo!();
+    let mul = a.iter().product::<f64>();
+    println!("{}", mul / a.iter().map(|x| mul / x).sum::<f64>());
 }
