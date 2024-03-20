d41eb2229285b1d533702a35b156dd95e6e4bc45
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-03-20 21:10:48 +0900

    refactor

--- abc340/src/bin/a.rs
+++ abc340/src/bin/a.rs
@@ -5,5 +5,5 @@ fn main() {
     input! {
         (a, b, d): (i32, i32, i32),
     }
-    println!("{}", (a..=b).filter(|v| (*v - a) % d == 0).join(" "));
+    println!("{}", (0..=(b - a) / d).map(|i| a + i * d).join(" "));
 }
