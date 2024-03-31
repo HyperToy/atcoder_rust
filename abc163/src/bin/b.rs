4db9d3f551edc6d5adbb4474f5fd20db3d5f3b42
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-03-31 09:02:10 +0900

    refactor

--- abc163/src/bin/b.rs
+++ abc163/src/bin/b.rs
@@ -5,6 +5,5 @@ fn main() {
         n: i32, m: usize,
         a: [i32; m],
     }
-    let total = a.iter().sum::<i32>();
-    println!("{}", if total > n { -1 } else { n - total });
+    println!("{}", (n - a.iter().sum::<i32>()).max(-1));
 }
