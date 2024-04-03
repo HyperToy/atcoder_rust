aee80bd7c086155fa1947615ac59766854b450d4
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-04-03 20:52:16 +0900

    shojin

--- abc310/src/bin/e.rs
+++ abc310/src/bin/e.rs
@@ -1,8 +1,30 @@
-use proconio::*;
+use proconio::{marker::Chars, *};
 
 fn main() {
     input! {
-        //
+        n: usize,
+        s: Chars,
     }
-    todo!();
+    let s = s
+        .iter()
+        .map(|c| if *c == '0' { 0 } else { 1 })
+        .collect::<Vec<_>>();
+    // dp[i].k := i 番目までみたときの、 k (=0,1) の個数
+    let mut dp = vec![(0, 0); n];
+    if s[0] == 0 {
+        dp[0].0 = 1;
+    } else {
+        dp[0].1 = 1;
+    }
+    for i in 1..n {
+        if s[i] == 0 {
+            dp[i].1 = dp[i - 1].0 + dp[i - 1].1;
+            dp[i].0 = 1;
+        } else {
+            dp[i].0 = dp[i - 1].1;
+            dp[i].1 = dp[i - 1].0 + 1;
+        }
+    }
+    let answer = dp.iter().map(|(_, x)| x).sum::<i64>();
+    println!("{}", answer);
 }
