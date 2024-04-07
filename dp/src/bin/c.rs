db4c22dcd19c719d800b044e431cb5dbfb9daf85
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-04-07 21:14:31 +0900

    edpc

--- dp/src/bin/c.rs
+++ dp/src/bin/c.rs
@@ -2,7 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        abc: [[i64; 3]; n],
     }
-    todo!();
+    let mut dp = vec![vec![std::i64::MIN; 3]; n];
+    dp[0] = abc[0].clone();
+    for i in 1..n {
+        for j in 0..3 {
+            for k in 0..3 {
+                if j != k {
+                    dp[i][j] = dp[i][j].max(dp[i - 1][k] + abc[i][j]);
+                }
+            }
+        }
+    }
+    println!("{}", dp[n - 1].iter().max().unwrap());
 }
