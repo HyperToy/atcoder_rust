aabc9e4520d1b4936c5a0063dfcbed5b57801277
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-04-09 23:30:45 +0900

    virtual

--- abc338/src/bin/a.rs
+++ abc338/src/bin/a.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        s: Chars,
     }
-    todo!();
+    println!(
+        "{}",
+        if s[0].is_uppercase() && s[1..].iter().all(|c| c.is_lowercase()) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
--- abc338/src/bin/b.rs
+++ abc338/src/bin/b.rs
@@ -1,8 +1,23 @@
-use proconio::input;
+use std::collections::BTreeMap;
+
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        s: Chars,
     }
-    todo!();
+    let mut count = BTreeMap::new();
+    for c in s {
+        *(count.entry(c).or_insert(0)) += 1;
+    }
+    // refactor
+    let mut answer = '.';
+    let mut max = 0;
+    for (c, x) in count {
+        if x > max {
+            max = x;
+            answer = c;
+        }
+    }
+    println!("{}", answer);
 }
