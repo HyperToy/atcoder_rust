08e5388482385e98284f7bf131aa37647634345b
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-04-10 16:26:38 +0900

    virtual

--- abc337/src/bin/a.rs
+++ abc337/src/bin/a.rs
@@ -1,8 +1,20 @@
 use proconio::input;
+use std::cmp::Ordering;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        xy: [(i32, i32); n],
     }
-    todo!();
+    let (takahashi, aoki) = xy
+        .into_iter()
+        .fold((0, 0), |(sx, sy), (x, y)| (sx + x, sy + y));
+    println!(
+        "{}",
+        match takahashi.cmp(&aoki) {
+            Ordering::Greater => "Takahashi",
+            Ordering::Less => "Aoki",
+            Ordering::Equal => "Draw",
+        }
+    );
 }
--- abc337/src/bin/c.rs
+++ abc337/src/bin/c.rs
@@ -1,8 +1,29 @@
+use itertools::Itertools;
 use proconio::input;
 
+// todo: refactor
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i32; n],
     }
-    todo!();
+    let mut g = vec![Vec::new(); n];
+    let mut start = 0;
+    for i in 0..n {
+        // a[i] -1 -> i の辺を張る
+        if a[i] > 0 {
+            g[a[i] as usize - 1].push(i);
+        } else {
+            start = i;
+        }
+    }
+    let mut answer = Vec::new();
+    dfs(start, &mut answer, &g);
+    println!("{}", answer.iter().map(|a| a + 1).join(" "));
+}
+fn dfs(p: usize, v: &mut Vec<usize>, g: &Vec<Vec<usize>>) {
+    v.push(p);
+    for u in &g[p] {
+        dfs(*u, v, g);
+    }
 }
