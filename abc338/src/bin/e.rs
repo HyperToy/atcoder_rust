dce93a894dedb59144e1d58653d9eb2b1ce70ce3
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-05-02 17:09:46 +0900

    shojin

--- abc338/src/bin/e.rs
+++ abc338/src/bin/e.rs
@@ -1,8 +1,28 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        n: usize,
+        ab: [(Usize1, Usize1); n],
     }
-    todo!();
+    let mut v = vec![None; n * 2];
+    for (a, b) in ab {
+        v[a.max(b)] = Some(a.min(b));
+    }
+    let mut s: Vec<usize> = Vec::new();
+    let mut exist = false;
+    for i in 0..n * 2 {
+        match v[i] {
+            Some(x) => {
+                if s.pop().unwrap() != x {
+                    exist = true;
+                    break;
+                }
+            }
+            None => {
+                s.push(i);
+            }
+        }
+    }
+    println!("{}", if exist { "Yes" } else { "No" });
 }
