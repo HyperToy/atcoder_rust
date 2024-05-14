89c6f21b21d3942f08b75e407186083e48fedb51
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-05-14 14:10:31 +0900

    shojin

--- abc351/src/bin/f.rs
+++ abc351/src/bin/f.rs
@@ -1,8 +1,22 @@
+use ac_library::FenwickTree;
+use itertools::Itertools;
 use proconio::input;
 
+// 平面走査
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i64; n],
     }
-    todo!();
+    let mut s0 = FenwickTree::new(n, 0); // 個数
+    let mut s1 = FenwickTree::new(n, 0); // 総和
+    let mut answer = 0;
+    for (i, a) in a.into_iter().enumerate().sorted_by_key(|p| p.1).rev() {
+        let c = s0.sum(i..);
+        let s = s1.sum(i..);
+        answer += s - c * a;
+        s0.add(i, 1);
+        s1.add(i, a);
+    }
+    println!("{}", answer);
 }
