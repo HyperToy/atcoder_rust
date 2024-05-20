efa73c46938dc13d08117f048e74fbc4de9c6cd3
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-05-20 23:31:25 +0900

    shojin

--- abc339/src/bin/e.rs
+++ abc339/src/bin/e.rs
@@ -1,8 +1,17 @@
+use ac_library::{Max, Segtree};
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, d: usize,
+        a: [usize; n],
     }
-    todo!();
+    let max = *a.iter().max().unwrap() + 1;
+    let mut segtree = Segtree::<Max<usize>>::new(max);
+    for a in a {
+        let l = if a >= d { a - d } else { 0 };
+        let r = if a + d < max { a + d + 1 } else { max };
+        segtree.set(a, segtree.prod(l..r) + 1);
+    }
+    println!("{}", segtree.prod(0..max));
 }
