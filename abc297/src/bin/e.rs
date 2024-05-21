7ac998d43ea6626e1898f7f03d52446003045d10
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-05-21 22:12:08 +0900

    shojin

--- abc297/src/bin/e.rs
+++ abc297/src/bin/e.rs
@@ -1,11 +1,21 @@
-use proconio::*;
-// use proconio::marker::Chars;
-// use proconio::marker::Bytes;
-// use itertools::Itertools;
+use proconio::input;
+use std::collections::BTreeSet;
 
 fn main() {
     input! {
-        //
+        n: usize, k: usize,
+        a: [i64; n],
     }
-    todo!();
+    let k = k + 1;
+    let mut s = BTreeSet::new();
+    s.insert(0);
+    let mut answer = vec![];
+    while answer.len() < k {
+        let v = s.pop_first().unwrap();
+        answer.push(v);
+        for &a in &a {
+            s.insert(v + a);
+        }
+    }
+    println!("{}", answer[k - 1]);
 }
