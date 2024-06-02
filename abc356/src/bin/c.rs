10891bd2d2247f2cdd1705b41e5467e32afa0388
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-06-02 10:05:30 +0900

    upsolve

--- abc356/src/bin/c.rs
+++ abc356/src/bin/c.rs
@@ -4,30 +4,28 @@ fn main() {
     input! {
         n: usize, m: usize, k: u32,
     }
-    let mut res = Vec::new();
-    for _ in 0..m {
-        input! {
-            c: usize,
-            a: [Usize1; c], r: char,
-        }
-        let mut now = 0;
-        for i in a {
-            now |= 1 << i;
-        }
-        res.push((
-            now,
-            match r {
-                'o' => true,
-                'x' => false,
-                _ => unreachable!(),
-            },
-        ));
-    }
+    let res = (0..m)
+        .map(|_| {
+            input! {
+                c: usize,
+                a: [Usize1; c],
+                r: char,
+            }
+            (
+                a.into_iter().map(|i| 1 << i).sum::<i32>(),
+                match r {
+                    'o' => true,
+                    'x' => false,
+                    _ => unreachable!(),
+                },
+            )
+        })
+        .collect::<Vec<_>>();
     println!(
         "{}",
         (0..(1 << n))
             .filter(|&mask| {
-                (0..m).all(|j| (((mask & (res[j].0)) as i32).count_ones() >= k) == res[j].1)
+                (0..m).all(|j| ((mask & res[j].0 as i32).count_ones() >= k) == res[j].1)
             })
             .count()
     );
--- abc356/src/bin/e.rs
+++ abc356/src/bin/e.rs
@@ -4,7 +4,32 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        a: [i64; n],
+        a: [usize; n],
     }
     let a = a.into_iter().sorted().collect::<Vec<_>>();
+    let m = *a.last().unwrap();
+    let mut c = vec![0; m + 1];
+    for &x in &a {
+        c[x] += 1;
+    }
+    let mut s = c.clone();
+    for i in 1..=m {
+        s[i] += s[i - 1];
+    }
+    let mut answer = 0;
+    for (d, c) in c.into_iter().enumerate() {
+        if c == 0 {
+            continue;
+        }
+        for n in 1..=m / d {
+            answer += n * f(&s, d, n) * c;
+        }
+        answer -= c * (c - 1) / 2;
+    }
+    answer -= n;
+    println!("{}", answer);
+}
+fn f(c: &Vec<usize>, d: usize, n: usize) -> usize {
+    let m = c.len() - 1;
+    c[m.min(d * (n + 1) - 1)] - c[d * n - 1]
 }
