0fcfb562813fb558d12efec7855519c164dbae58
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-06-11 09:34:15 +0900

    shojin

--- abc326/src/bin/e.rs
+++ abc326/src/bin/e.rs
@@ -1,8 +1,18 @@
+use ac_library::ModInt998244353 as Mint;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i32; n],
     }
-    todo!();
+    let mut e = Mint::from(0);
+    let mut sum_e = Mint::from(0);
+    let mut sum_a = Mint::from(0);
+    for i in (0..n).rev() {
+        sum_e += e;
+        sum_a += a[i];
+        e = (sum_e + sum_a) / n;
+    }
+    println!("{}", e);
 }
