3781a764f4b9ec1d63c0d24bca0c29864aaf1423
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-06-21 10:28:51 +0900

    shojin

--- abc294/src/bin/a.rs
+++ abc294/src/bin/a.rs
@@ -1,8 +1,10 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i32; n],
     }
-    todo!();
+    println!("{}", a.into_iter().filter(|&x| x % 2 == 0).join(" "));
 }
