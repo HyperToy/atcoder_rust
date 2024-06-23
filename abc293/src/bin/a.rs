0a0f65771164f8b31a74084d0d5aa787acdc7917
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-06-23 23:51:30 +0900

    shojin

--- abc293/src/bin/a.rs
+++ abc293/src/bin/a.rs
@@ -1,8 +1,14 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        s: Chars,
     }
-    todo!();
+    let mut t = Vec::new();
+    for i in 0..s.len() / 2 {
+        t.push(s[i * 2 + 1]);
+        t.push(s[i * 2]);
+    }
+    println!("{}", t.into_iter().join(""));
 }
