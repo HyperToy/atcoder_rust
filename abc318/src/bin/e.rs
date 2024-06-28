810832676312359e0ac6746eb14633facc279f31
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-06-28 14:25:41 +0900

    shojin with editorial

--- abc318/src/bin/e.rs
+++ abc318/src/bin/e.rs
@@ -1,8 +1,25 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [Usize1; n],
     }
-    todo!();
+    let mut answer = 0i64;
+    let mut mul = 0;
+    let mut left = vec![0; n];
+    let mut right = vec![0; n];
+    for i in 0..n {
+        right[a[i]] += 1;
+    }
+    for i in 0..n {
+        right[a[i]] -= 1;
+        mul -= left[a[i]];
+        if i > 0 {
+            left[a[i - 1]] += 1;
+            mul += right[a[i - 1]];
+        }
+        answer += mul - left[a[i]] * right[a[i]];
+    }
+    println!("{}", answer);
 }
