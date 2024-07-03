fad71cbae681bc81ba0acc0abea84e92e91367cd
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-03 10:26:30 +0900

    shojin

--- abc351/src/bin/d.rs
+++ abc351/src/bin/d.rs
@@ -1,7 +1,6 @@
-// todo
 use itertools::Itertools;
 use proconio::{input, marker::Chars};
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 
 const DY: [isize; 4] = [0, 1, 0, -1];
 const DX: [isize; 4] = [1, 0, -1, 0];
@@ -10,65 +9,81 @@ fn main() {
         (h, w): (usize, usize),
         s: [Chars; h],
     }
-    let free = free(h, w, &s); // 自由に動けるマスか
+    let statuses = cell_statuses(h, w, &s);
     let mut answer = 1;
-    let mut seen = vec![vec![false; w]; h];
     let mut q = VecDeque::new();
-    let mut visited;
+    let mut visited = vec![vec![false; w]; h];
     for (i, j) in (0..h).cartesian_product(0..w) {
-        if !free[i][j] || seen[i][j] {
+        if statuses[i][j] != CellStatus::Free || visited[i][j] {
             continue;
         }
-        visited = vec![vec![false; w]; h];
         q.push_back((i, j));
         visited[i][j] = true;
         let mut now = 0;
-        while !q.is_empty() {
-            now += 1;
-            let (y, x) = q.pop_front().unwrap();
-            if !free[y][x] {
-                continue;
-            }
-            seen[y][x] = true;
-            for k in 0..4 {
-                let ny = y as isize + DY[k];
-                let nx = x as isize + DX[k];
-                if !inside(ny, nx, h, w) {
-                    continue;
+        let mut bound_cells = HashSet::new();
+        while let Some((y, x)) = q.pop_front() {
+            match statuses[y][x] {
+                CellStatus::Magnet => (),
+                CellStatus::Bound => {
+                    now += 1;
                 }
-                let (ny, nx) = (ny as usize, nx as usize);
-                if visited[ny][nx] {
-                    continue;
+                CellStatus::Free => {
+                    now += 1;
+                    for (ny, nx) in neighbors(y, x, h, w) {
+                        if visited[ny][nx] {
+                            continue;
+                        }
+                        q.push_back((ny, nx));
+                        visited[ny][nx] = true;
+                        if statuses[ny][nx] == CellStatus::Bound {
+                            bound_cells.insert((ny, nx));
+                        }
+                    }
                 }
-                q.push_back((ny, nx));
-                visited[ny][nx] = true;
             }
         }
         answer = answer.max(now);
+        for (y, x) in bound_cells {
+            visited[y][x] = false;
+        }
     }
     println!("{}", answer);
 }
-fn free(h: usize, w: usize, s: &Vec<Vec<char>>) -> Vec<Vec<bool>> {
-    let mut free = vec![vec![true; w]; h];
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum CellStatus {
+    Magnet,
+    Bound,
+    Free,
+}
+fn cell_statuses(h: usize, w: usize, s: &Vec<Vec<char>>) -> Vec<Vec<CellStatus>> {
+    let mut res = vec![vec![CellStatus::Free; w]; h];
     for (i, j) in (0..h).cartesian_product(0..w) {
         if s[i][j] == '#' {
-            free[i][j] = false;
+            res[i][j] = CellStatus::Magnet;
             continue;
         }
-        for k in 0..4 {
-            let ni = i as isize + DY[k];
-            let nj = j as isize + DX[k];
-            if !inside(ni, nj, h, w) {
-                continue;
-            }
-            let (ni, nj) = (ni as usize, nj as usize);
+        for (ni, nj) in neighbors(i, j, h, w) {
             if s[ni][nj] == '#' {
-                free[i][j] = false;
+                res[i][j] = CellStatus::Bound;
                 break;
             }
         }
     }
-    free
+    res
+}
+fn neighbors(y: usize, x: usize, h: usize, w: usize) -> Vec<(usize, usize)> {
+    let mut res = Vec::new();
+    for k in 0..4 {
+        let ny = y as isize + DY[k];
+        let nx = x as isize + DX[k];
+        if !inside(ny, nx, h, w) {
+            continue;
+        }
+        let (ny, nx) = (ny as usize, nx as usize);
+        res.push((ny, nx));
+    }
+    res
 }
 fn inside(y: isize, x: isize, h: usize, w: usize) -> bool {
     0 <= y && y < h as isize && 0 <= x && x < w as isize
