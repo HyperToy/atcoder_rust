16474e2a9ed402bc50d1251d829fd552063ff136
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-13 21:01:12 +0900

    shojin

--- arc164/src/bin/b.rs
+++ arc164/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use ac_library::Dsu;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        es: [(Usize1, Usize1); m],
+        c: [i32; n],
     }
-    todo!();
+    let mut dsu = Dsu::new(n);
+    for &(a, b) in &es {
+        if c[a] != c[b] {
+            dsu.merge(a, b);
+        }
+    }
+    println!(
+        "{}",
+        if es.iter().any(|&(a, b)| c[a] == c[b] && dsu.same(a, b)) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
