25fa7c527200990c61f818426bdc7419015c6999
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-14 07:12:05 +0900

    in contest

--- abc362/src/bin/a.rs
+++ abc362/src/bin/a.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        r: i32, g: i32, b: i32,
+        c: String
     }
-    todo!();
+    println!(
+        "{}",
+        match c.as_str() {
+            "Red" => g.min(b),
+            "Green" => b.min(r),
+            "Blue" => r.min(g),
+            _ => unreachable!(),
+        }
+    );
 }
--- abc362/src/bin/d.rs
+++ abc362/src/bin/d.rs
@@ -1,8 +1,37 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        a: [i64; n],
+        es: [((Usize1, Usize1), i64); m],
     }
-    todo!();
+    let mut g = vec![Vec::new(); n];
+    for ((u, v), w) in es {
+        g[u].push((v, w));
+        g[v].push((u, w));
+    }
+    let g = g;
+
+    let mut dist = vec![std::i64::MAX; n];
+    let mut q = BinaryHeap::new();
+    let start = 0;
+    dist[start] = a[start];
+    q.push((Reverse(dist[start]), start));
+    while let Some((Reverse(d), u)) = q.pop() {
+        if dist[u] < d {
+            continue;
+        }
+        for &(v, w) in &g[u] {
+            let nd = d + w + a[v];
+            if dist[v] <= nd {
+                continue;
+            }
+            dist[v] = nd;
+            q.push((Reverse(nd), v));
+        }
+    }
+    println!("{}", dist.into_iter().skip(1).join(" "));
 }
