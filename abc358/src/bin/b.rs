78e37e2011977f1b6b9a332b91588fc1a18433f5
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-17 14:27:41 +0900

    shojin

--- abc358/src/bin/b.rs
+++ abc358/src/bin/b.rs
@@ -1,8 +1,17 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, a: i32,
+        t: [i32; n],
     }
-    todo!();
+    let mut time = 0;
+    let mut answer = vec![];
+    for t in t {
+        time = time.max(t);
+        time += a;
+        answer.push(time);
+    }
+    println!("{}", answer.iter().join("\n"));
 }
--- abc358/src/bin/c.rs
+++ abc358/src/bin/c.rs
@@ -1,8 +1,39 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        n: usize, m: u32,
+        s: [Chars; n],
     }
-    todo!();
+    let s = s
+        .iter()
+        .map(|cs| {
+            cs.iter()
+                .map(|&c| match c {
+                    'o' => 1,
+                    'x' => 0,
+                    _ => unreachable!(),
+                })
+                .enumerate()
+                .map(|(i, v)| v << i)
+                .sum::<usize>()
+        })
+        .collect_vec();
+    println!(
+        "{}",
+        (0..1usize << n)
+            .filter(|&msk| {
+                let mut any = 0;
+                for i in 0..n {
+                    if (msk >> i) & 1 == 1 {
+                        any |= s[i];
+                    }
+                }
+                any.count_ones() == m
+            })
+            .map(|msk| msk.count_ones())
+            .min()
+            .unwrap()
+    )
 }
