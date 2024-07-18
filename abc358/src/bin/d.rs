bc372b0e8294fdf28c3d91e437c11fe3d35758f4
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-18 20:36:29 +0900

    shojin

--- abc358/src/bin/d.rs
+++ abc358/src/bin/d.rs
@@ -1,8 +1,25 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        a: [i64; n],
+        b: [i64; m],
     }
-    todo!();
+    let a = a.into_iter().sorted().collect_vec();
+    let b = b.into_iter().sorted().collect_vec();
+    let mut i = 0;
+    let mut j = 0;
+    let mut answer = 0i64;
+    while i < n && j < m {
+        if a[i] >= b[j] {
+            answer += a[i];
+            i += 1;
+            j += 1;
+        } else {
+            i += 1;
+        }
+    }
+    println!("{}", if j == m { answer } else { -1 });
 }
