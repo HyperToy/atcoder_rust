f122f2b58e0d1aaba0d642399f7be215c635d4ce
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-22 21:49:30 +0900

    shojin

--- abc363/src/bin/c.rs
+++ abc363/src/bin/c.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        n: usize, k: usize,
+        s: Chars,
     }
-    todo!();
+    println!(
+        "{}",
+        s.into_iter()
+            .sorted()
+            .permutations(n)
+            .sorted()
+            .dedup()
+            .filter(|s| s.windows(k).all(|s| !(0..k).all(|i| s[i] == s[k - 1 - i])))
+            .count()
+    );
 }
