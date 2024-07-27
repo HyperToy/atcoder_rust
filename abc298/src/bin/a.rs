5535ff21090be9ff88e06b378cbbc03458fb9cd0
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-27 11:19:45 +0900

    shojin

--- abc298/src/bin/a.rs
+++ abc298/src/bin/a.rs
@@ -1,8 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        _n: usize,
+        s: Chars,
     }
-    todo!();
+    println!(
+        "{}",
+        if s.iter().all(|&c| c != 'x') && s.iter().any(|&c| c == 'o') {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
