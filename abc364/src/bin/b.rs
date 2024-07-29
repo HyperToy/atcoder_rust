3a1fd95af72e056fc807cc6485f7fdc46f56844a
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-07-29 11:46:01 +0900

    shojin

--- abc364/src/bin/b.rs
+++ abc364/src/bin/b.rs
@@ -1,8 +1,32 @@
-use proconio::input;
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
     input! {
-        //
+        h: usize, w: usize,
+        s: (Usize1, Usize1),
+        c: [Chars; h],
+        x: Chars,
     }
-    todo!();
+    let mut pos = s;
+    for d in x {
+        let next = apply(pos, h, w, d);
+        if c[next.0][next.1] == '.' {
+            pos = next;
+        }
+    }
+    println!("{} {}", pos.0 + 1, pos.1 + 1);
+}
+
+fn apply(pos: (usize, usize), h: usize, w: usize, dir: char) -> (usize, usize) {
+    let (i, j) = pos;
+    match dir {
+        'L' => (i, if j > 0 { j - 1 } else { j }),
+        'R' => (i, if j + 1 < w { j + 1 } else { j }),
+        'U' => (if i > 0 { i - 1 } else { i }, j),
+        'D' => (if i + 1 < h { i + 1 } else { i }, j),
+        _ => unreachable!(),
+    }
 }
--- abc364/src/bin/c.rs
+++ abc364/src/bin/c.rs
@@ -1,8 +1,29 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, x: i64, y: i64,
+        a: [i64; n],
+        b: [i64; n],
     }
-    todo!();
+    let mut ca = 0;
+    let mut sa = 0;
+    let mut cb = 0;
+    let mut sb = 0;
+    for a in a.iter().sorted().rev() {
+        sa += a;
+        ca += 1;
+        if sa > x {
+            break;
+        }
+    }
+    for b in b.iter().sorted().rev() {
+        sb += b;
+        cb += 1;
+        if sb > y {
+            break;
+        }
+    }
+    println!("{}", ca.min(cb));
 }
