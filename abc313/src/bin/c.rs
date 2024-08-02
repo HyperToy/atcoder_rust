d1e04bf9e4f919aed19f40df76935f3a88d8ecfc
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-02 15:09:54 +0900

    shojin

--- abc313/src/bin/c.rs
+++ abc313/src/bin/c.rs
@@ -2,7 +2,21 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [usize; n],
     }
-    todo!();
+    let sum = a.iter().sum::<usize>();
+    let min = sum / n;
+    let max = min + if sum % n == 0 { 0 } else { 1 };
+    let mut answer = 0;
+    let mut answer2 = 0;
+    for x in a {
+        if x < min {
+            answer += min - x;
+        }
+        if x > max {
+            answer2 += x - max;
+        }
+    }
+    println!("{}", answer.max(answer2));
 }
