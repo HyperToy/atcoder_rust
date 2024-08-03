3069651815c6fa5f3b7e38e926b494d83c90f0fc
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-03 12:14:36 +0900

    shojin

--- abc319/src/bin/d.rs
+++ abc319/src/bin/d.rs
@@ -2,7 +2,27 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        l: [usize; n],
     }
-    todo!();
+    let mut ng = l.iter().max().unwrap() - 1;
+    let mut ok = l.iter().sum::<usize>() + n - 1;
+    while ng + 1 < ok {
+        let wj = (ng + ok) / 2;
+        let mut h = 1;
+        let mut now = 0;
+        for &x in &l {
+            if now + x > wj {
+                h += 1;
+                now = 0;
+            }
+            now += x + 1;
+        }
+        if h <= m {
+            ok = wj;
+        } else {
+            ng = wj;
+        }
+    }
+    println!("{ok}");
 }
