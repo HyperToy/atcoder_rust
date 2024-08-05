c3bd4b7c972f2e40a4f525988e1485c3054c7624
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-05 14:13:28 +0900

    shojin

--- abc365/src/bin/c.rs
+++ abc365/src/bin/c.rs
@@ -2,7 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, m: i64,
+        a: [i64; n],
     }
-    todo!();
+    if a.iter().sum::<i64>() <= m {
+        println!("infinite");
+        return;
+    }
+    let mut ok = 0;
+    let mut ng = m;
+    while ok + 1 < ng {
+        let wj = (ok + ng) / 2;
+        if a.iter().map(|&x| x.min(wj)).sum::<i64>() <= m {
+            ok = wj;
+        } else {
+            ng = wj;
+        }
+    }
+    println!("{}", ok);
 }
