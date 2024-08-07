5e92700b9dc64c77da23373d8d0bbaa93c716328
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-07 15:13:27 +0900

    refactor

--- abc365/src/bin/e.rs
+++ abc365/src/bin/e.rs
@@ -6,16 +6,23 @@ fn main() {
         n: usize,
         a: [usize; n],
     }
-    let mut answer = 0;
-    for d in 0..30 {
-        let b = a.iter().map(|&x| (x >> d) & 1).collect_vec();
-        let mut c = vec![0];
-        for &x in &b {
-            c.push(c.last().unwrap() ^ x);
-        }
-        let now = c.iter().filter(|&&x| x == 0).count() * c.iter().filter(|&&x| x == 1).count()
-            - b.iter().sum::<usize>();
-        answer += now << d;
-    }
-    println!("{}", answer);
+    println!(
+        "{}",
+        (0..30)
+            .map(|d| {
+                let b = a.iter().map(|&x| (x >> d) & 1).collect_vec();
+                let c = b
+                    .iter()
+                    .scan(0, |s, &x| {
+                        *s = *s ^ x;
+                        Some(*s)
+                    })
+                    .collect_vec();
+                let now = (c.iter().filter(|&&x| x == 0).count() + 1)
+                    * c.iter().filter(|&&x| x == 1).count()
+                    - b.iter().sum::<usize>();
+                now << d
+            })
+            .sum::<usize>()
+    );
 }
