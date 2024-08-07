7b1aefed207ff8ed6a7b2c2dab58472337df6642
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-07 11:56:12 +0900

    shojin

--- abc365/src/bin/d.rs
+++ abc365/src/bin/d.rs
@@ -1,8 +1,48 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
+
+type Hand = usize;
+const R: Hand = 0;
+const P: Hand = 1;
+const S: Hand = 2;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        s: Chars,
     }
-    todo!();
+    let s = s
+        .iter()
+        .map(|&c| match c {
+            'R' => R,
+            'P' => P,
+            'S' => S,
+            _ => unreachable!(),
+        })
+        .collect_vec();
+    let mut dp = vec![[std::i32::MIN; 3]; n];
+    dp[0][win(s[0])] = 1;
+    dp[0][s[0]] = 0;
+    for i in 1..n {
+        for j in 0..3 {
+            if s[i] == win(j) {
+                continue;
+            }
+            for k in 0..3 {
+                if k == j {
+                    continue;
+                }
+                dp[i][j] = dp[i][j].max(dp[i - 1][k] + if j == win(s[i]) { 1 } else { 0 });
+            }
+        }
+    }
+    println!("{}", dp[n - 1].iter().max().unwrap());
+}
+fn win(h: Hand) -> Hand {
+    match h {
+        R => P,
+        P => S,
+        S => R,
+        _ => unreachable!(),
+    }
 }
--- abc365/src/bin/e.rs
+++ abc365/src/bin/e.rs
@@ -1,8 +1,21 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [usize; n],
     }
-    todo!();
+    let mut answer = 0;
+    for d in 0..30 {
+        let b = a.iter().map(|&x| (x >> d) & 1).collect_vec();
+        let mut c = vec![0];
+        for &x in &b {
+            c.push(c.last().unwrap() ^ x);
+        }
+        let now = c.iter().filter(|&&x| x == 0).count() * c.iter().filter(|&&x| x == 1).count()
+            - b.iter().sum::<usize>();
+        answer += now << d;
+    }
+    println!("{}", answer);
 }
