5cc67a9aca012301b53337629ba6edf92cfbc053
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-11 07:01:42 +0900

    shojin

--- abc287/src/bin/a.rs
+++ abc287/src/bin/a.rs
@@ -2,7 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        s: [String; n],
     }
-    todo!();
+    println!(
+        "{}",
+        if s.iter().filter(|&s| s == "For").count() > n / 2 {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
