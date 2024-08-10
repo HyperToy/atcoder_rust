08f75d45ee60ef041877dbfb0519171c3d20371f
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-10 18:42:54 +0900

    shojin

--- abc298/src/bin/b.rs
+++ abc298/src/bin/b.rs
@@ -1,8 +1,30 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [[i32; n]; n],
+        b: [[i32; n]; n],
     }
-    todo!();
+    let turn = |(i, j)| (n - 1 - j, i);
+    println!(
+        "{}",
+        if (0..4).any(|x| {
+            (0..n)
+                .cartesian_product(0..n)
+                .map(|p| {
+                    let mut q = p;
+                    for _ in 0..x {
+                        q = turn(q)
+                    }
+                    (q, p)
+                })
+                .all(|((i, j), (k, l))| a[i][j] == 0 || b[k][l] == 1)
+        }) {
+            "Yes"
+        } else {
+            "No"
+        }
+    );
 }
