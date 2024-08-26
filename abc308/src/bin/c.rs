87d515e343c6df7f89c020ab00c6446730f5ce7d
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-26 10:29:23 +0900

    shojin

--- abc308/src/bin/c.rs
+++ abc308/src/bin/c.rs
@@ -1,8 +1,26 @@
+use itertools::Itertools;
 use proconio::input;
+use std::cmp::Ordering;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        ab: [(i64, i64); n],
     }
-    todo!();
+    println!(
+        "{}",
+        ab.iter()
+            .zip(0..n)
+            .sorted_by(|((a1, b1), i), ((a2, b2), j)| {
+                let c = (a1 * (a2 + b2)).cmp(&(a2 * (a1 + b1)));
+                if c == Ordering::Equal {
+                    j.cmp(&i)
+                } else {
+                    c
+                }
+            })
+            .rev()
+            .map(|((_a, _b), i)| i + 1)
+            .join(" ")
+    );
 }
