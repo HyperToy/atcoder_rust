b00cdaefe700aba34a9db1920832c85f846ced34
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-28 09:23:56 +0900

    shojin

--- abc300/src/bin/c.rs
+++ abc300/src/bin/c.rs
@@ -1,8 +1,40 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        h: usize, w: usize,
+        s: [Chars; h],
     }
-    todo!();
+    let n = h.min(w);
+    let mut answer = vec![0; n + 1];
+    for i in 0..h {
+        for j in 0..w {
+            if s[i][j] != '#' {
+                continue;
+            }
+            for l in 1..=n {
+                let nbs = neighbors(i, j, l, h, w);
+                if nbs.len() != 4 || nbs.iter().any(|&(y, x)| s[y][x] != '#') {
+                    answer[l - 1] += 1;
+                    break;
+                }
+            }
+        }
+    }
+    println!("{}", answer.iter().skip(1).join(" "));
+}
+
+fn neighbors(i: usize, j: usize, l: usize, h: usize, w: usize) -> Vec<(usize, usize)> {
+    [(1, 1), (1, -1), (-1, -1), (-1, 1)]
+        .iter()
+        .map(|&(dy, dx)| (i as isize + dy * l as isize, j as isize + dx * l as isize))
+        .filter_map(|(ni, nj)| {
+            if 0 <= ni && ni < h as isize && 0 <= nj && nj < w as isize {
+                Some((ni as usize, nj as usize))
+            } else {
+                None
+            }
+        })
+        .collect_vec()
 }
