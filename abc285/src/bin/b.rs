12da712c0094197df7d252e8fa7e1f96602dba92
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-08-30 16:43:49 +0900

    shojin

--- abc285/src/bin/b.rs
+++ abc285/src/bin/b.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        n: usize,
+        s: Chars,
     }
-    todo!();
+    println!(
+        "{}",
+        (1..n)
+            .map(|i| (0..n - i).take_while(|&k| s[k] != s[k + i]).count())
+            .join("\n")
+    );
 }
