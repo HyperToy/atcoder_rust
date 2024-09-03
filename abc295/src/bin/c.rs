ca31156bffa732eb724de68d067f12d15596c485
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-03 16:57:54 +0900

    shojin

--- abc295/src/bin/c.rs
+++ abc295/src/bin/c.rs
@@ -1,8 +1,17 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i32; n],
     }
-    todo!();
+    println!(
+        "{}",
+        a.into_iter()
+            .counts()
+            .into_iter()
+            .map(|(_, v)| v / 2)
+            .sum::<usize>()
+    );
 }
