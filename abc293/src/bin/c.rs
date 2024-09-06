7c73b97b5ce323b1ae51b7c72640e0881a9577c3
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-06 09:20:15 +0900

    shojin

--- abc293/src/bin/c.rs
+++ abc293/src/bin/c.rs
@@ -1,8 +1,29 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        h: usize, w: usize,
+        a: [[i32; w]; h],
     }
-    todo!();
+    println!("{}", rec(0, 0, h, w, &a, &mut vec![]));
+}
+
+fn rec(i: usize, j: usize, h: usize, w: usize, a: &Vec<Vec<i32>>, v: &mut Vec<i32>) -> usize {
+    let mut res = 0;
+    v.push(a[i][j]);
+    if i == h - 1 && j == w - 1 {
+        if v.iter().sorted().dedup().count() == h + w - 1 {
+            res += 1;
+        }
+    } else {
+        if i + 1 < h {
+            res += rec(i + 1, j, h, w, a, v);
+        }
+        if j + 1 < w {
+            res += rec(i, j + 1, h, w, a, v);
+        }
+    }
+    v.pop();
+    res
 }
--- zone2021/src/bin/a.rs
+++ zone2021/src/bin/a.rs
@@ -6,8 +6,8 @@ fn main() {
     }
     let l: usize = 12;
     let mut ans: u32 = 0;
-    for i in 0..l-3 {
-        if &s[i..i+4] == "ZONe" {
+    for i in 0..l - 3 {
+        if &s[i..i + 4] == "ZONe" {
             ans += 1;
         }
     }
