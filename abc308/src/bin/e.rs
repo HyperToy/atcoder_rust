eca5ddd7d7f47daef81ca6a446d0de05af7df415
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-08 00:07:26 +0900

    別解

--- abc308/src/bin/e.rs
+++ abc308/src/bin/e.rs
@@ -29,4 +29,26 @@ fn main() {
             .map(|(mask, count)| count * (0..).find(|&i| (mask >> i) & 1 == 0).unwrap())
             .sum::<i64>()
     );
+    /* 別解
+    use std::collections::HashMap;
+    let mut dp = HashMap::new();
+    dp.insert((0, 0), 1);
+    for (&a, &s) in a.iter().zip(s.iter()) {
+        let mut next_dp = HashMap::new();
+        for (&(j, mask), &count) in &dp {
+            *(next_dp.entry((j, mask)).or_insert(0)) += count;
+            if j < l && s == t[j] {
+                *(next_dp.entry((j + 1, mask | 1 << a)).or_insert(0)) += count;
+            }
+        }
+        dp = next_dp;
+    }
+    println!(
+        "{}",
+        dp.iter()
+            .filter(|(&(j, _), _)| j == 3)
+            .map(|((_, mask), count)| count * (0..).find(|&i| (mask >> i) & 1 == 0).unwrap())
+            .sum::<i64>()
+    );
+    */
 }
