ea2dc018cf00f67ab12864ae7bd038cdd8407a68
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-08 15:58:20 +0900

    shojin

--- abc294/src/bin/c.rs
+++ abc294/src/bin/c.rs
@@ -1,8 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
+use std::iter::repeat;
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        a: [i32; n],
+        b: [i32; m],
     }
-    todo!();
+    println!(
+        "{}",
+        a.iter()
+            .zip(repeat('a'))
+            .chain(b.iter().zip(repeat('b')))
+            .sorted()
+            .enumerate()
+            .sorted_by_key(|&(_, (&x, c))| (c, x))
+            .map(|(i, _)| i + 1)
+            .join(" ")
+    );
 }
