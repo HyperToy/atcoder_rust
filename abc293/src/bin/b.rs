75475b251fb35921ff07b0015e8c8ad255afe643
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-10 11:05:25 +0900

    shojin

--- abc293/src/bin/b.rs
+++ abc293/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
+use std::collections::HashSet;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [Usize1; n],
     }
-    todo!();
+    let mut called = HashSet::new();
+    for (i, &a) in a.iter().enumerate() {
+        if !called.contains(&i) {
+            called.insert(a);
+        }
+    }
+    println!(
+        "{}\n{}",
+        n - called.len(),
+        (0..n)
+            .filter(|&i| !called.contains(&i))
+            .map(|i| i + 1)
+            .join(" ")
+    );
 }
--- abc294/src/bin/d.rs
+++ abc294/src/bin/d.rs
@@ -1,8 +1,29 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
+use std::collections::BTreeSet;
 
 fn main() {
     input! {
-        //
+        n: usize, q: usize,
     }
-    todo!();
+    let mut waiting = (0..n).collect::<BTreeSet<_>>();
+    let mut called = BTreeSet::new();
+    let mut answer = vec![];
+    for _ in 0..q {
+        input! { t: usize }
+        match t {
+            1 => {
+                called.insert(waiting.pop_first().unwrap());
+            }
+            2 => {
+                input! { x: Usize1 }
+                called.remove(&x);
+            }
+            3 => {
+                answer.push(*called.first().unwrap());
+            }
+            _ => unreachable!(),
+        }
+    }
+    println!("{}", answer.iter().map(|&x| x + 1).join("\n"));
 }
