72f8e15304878467fa51bb75953c4376b549e177
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-14 11:36:57 +0900

    shojin

--- abc370/src/bin/b.rs
+++ abc370/src/bin/b.rs
@@ -1,8 +1,17 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [Usize1; n * (n + 1) / 2],
     }
-    todo!();
+    let mut g = vec![vec![0; n]; n];
+    for (a, (i, j)) in a
+        .into_iter()
+        .zip((0..n).map(|i| (0..=i).map(move |j| (i, j))).flatten())
+    {
+        g[i][j] = a;
+        g[j][i] = a;
+    }
+    println!("{}", (0..n).fold(0, |a, i| g[a][i]) + 1);
 }
