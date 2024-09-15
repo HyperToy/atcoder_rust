ac64b8d1550dd2cef5e8c5d2a894ed86edab0632
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-15 09:22:28 +0900

    shojin

--- abc369/src/bin/a.rs
+++ abc369/src/bin/a.rs
@@ -2,7 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        a: i32, b: i32,
     }
-    todo!();
+    let (a, b) = if a < b { (a, b) } else { (b, a) };
+    println!(
+        "{}",
+        (2 * a - b..=2 * b - a)
+            .filter(|&x| x * 2 == a + b || a * 2 == b + x || b * 2 == x + a)
+            .count()
+    );
 }
