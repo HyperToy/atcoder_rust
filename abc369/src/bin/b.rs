c84fbf7094212547d31087f81b676ac0a4ff7597
Ryohey <ryohey.trumpet+tech@gmail.com>
2024-09-17 06:22:17 +0900

    shojin

--- abc369/src/bin/b.rs
+++ abc369/src/bin/b.rs
@@ -2,7 +2,23 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        as_: [(i32, char); n],
     }
-    todo!();
+    let mut answer = 0;
+    let mut pl = None;
+    let mut pr = None;
+    for (a, s) in as_ {
+        let pre = match s {
+            'L' => &mut pl,
+            'R' => &mut pr,
+            _ => unreachable!(),
+        };
+        answer += match pre {
+            None => 0,
+            Some(x) => (&*x - a).abs(),
+        };
+        *pre = Some(a);
+    }
+    println!("{}", answer);
 }
