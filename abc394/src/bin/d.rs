8caee49aeabb7862c3a29ddcea536df814e6c8b8
ryohei.noda <ryohey.trumpet@gmail.com>
2025-03-08 21:12:53 +0900

    abc394_d

--- abc394/src/bin/d.rs
+++ abc394/src/bin/d.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
-    input! {
-        //
+    input! { s: Chars }
+    let mut st = Vec::new();
+    for c in s {
+        if st.last().is_some_and(|&x| fit(x, c)) {
+            st.pop();
+        } else {
+            st.push(c);
+        }
     }
-    todo!();
+    println!("{}", if st.len() == 0 { "Yes" } else { "No" });
+}
+fn fit(x: char, c: char) -> bool {
+    match (x, c) {
+        ('(', ')') | ('[', ']') | ('<', '>') => true,
+        _ => false,
+    }
 }
