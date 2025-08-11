77f95af330d4e70afb5934251ec5cc7b18f72650
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-11 12:50:40 +0900

    refactor: abc375_c

--- abc375/src/bin/c.rs
+++ abc375/src/bin/c.rs
@@ -11,7 +11,8 @@ fn main() {
         (0..n)
             .map(|i| {
                 (0..n)
-                    .map(move |j| rotate_by_count(n, (i, j), distance_from_edge(n, (i, j))))
+                    .map(move |j| (i, j))
+                    .map(|p| rotate_by_count(n, p))
                     .map(|(i, j)| a[i][j])
                     .join("")
             })
@@ -19,11 +20,8 @@ fn main() {
     );
 }
 
-fn distance_from_edge(n: usize, (i, j): (usize, usize)) -> usize {
-    usize::min(usize::min(i + 1, n - i), usize::min(j + 1, n - j)) % 4
-}
-
-fn rotate_by_count(n: usize, p: (usize, usize), count: usize) -> (usize, usize) {
+fn rotate_by_count(n: usize, p: (usize, usize)) -> (usize, usize) {
+    let count = distance_from_edge(n, p);
     let r = |p: (usize, usize)| rotate(n, p);
     match count {
         0 => p,
@@ -32,6 +30,10 @@ fn rotate_by_count(n: usize, p: (usize, usize), count: usize) -> (usize, usize) 
         3 => r(r(r(p))),
         _ => unreachable!(),
     }
+}
+
+fn distance_from_edge(n: usize, (i, j): (usize, usize)) -> usize {
+    usize::min(usize::min(i + 1, n - i), usize::min(j + 1, n - j)) % 4
 }
 
 fn rotate(n: usize, (i, j): (usize, usize)) -> (usize, usize) {
