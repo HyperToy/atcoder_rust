0f9fef01e33ad7993de566072741f27f46922e4b
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-14 08:29:38 +0900

    abc395_a

--- abc395/src/bin/a.rs
+++ abc395/src/bin/a.rs
@@ -1,8 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [i32; n],
     }
-    todo!();
+    print_yes_no(a.iter().tuple_windows().all(|(a, b)| a < b));
+}
+
+fn print_yes_no(b: bool) {
+    println!("{}", if b { "Yes" } else { "No" });
 }
