98048fc8b7af0cdbc519146ff562017ab7368cde
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-13 22:39:33 +0900

    feat(abc409): A問題の解答を実装

--- abc409/src/bin/a.rs
+++ abc409/src/bin/a.rs
@@ -1,8 +1,14 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        _n: usize,
+        t: Chars,
+        a: Chars,
     }
-    todo!();
+    print_yes_no(t.iter().zip(a.iter()).any(|(t, a)| t == a && t == &'o'));
+}
+
+fn print_yes_no(b: bool) {
+    println!("{}", if b { "Yes" } else { "No" });
 }
