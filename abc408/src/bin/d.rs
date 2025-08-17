843a657df68dded5cd43f0172555718ed2536282
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-17 11:06:29 +0900

    abc408_d

--- abc408/src/bin/d.rs
+++ abc408/src/bin/d.rs
@@ -1,8 +1,38 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        t: usize,
+        qs: [(usize, Chars); t],
     }
-    todo!();
+    println!("{}", qs.into_iter().map(|(_, q)| solve(q)).join("\n"));
+}
+
+fn solve(q: Vec<char>) -> i32 {
+    let mut dp = [Some(0), None, None];
+    for c in q {
+        let ndp = [
+            dp[0].map(|v| v + cost(c == '0')),
+            min_option(dp[0], dp[1]).map(|v| v + cost(c == '1')),
+            min_option(dp[1], dp[2]).map(|v| v + cost(c == '0')),
+        ];
+        dp = ndp;
+    }
+    dp.iter().filter_map(|&x| x).min().unwrap()
+}
+
+fn min_option(a: Option<i32>, b: Option<i32>) -> Option<i32> {
+    match (a, b) {
+        (Some(x), Some(y)) => Some(x.min(y)),
+        _ => a.or(b),
+    }
+}
+
+fn cost(b: bool) -> i32 {
+    if b {
+        0
+    } else {
+        1
+    }
 }
