3ef91dddc746fc2f1d83b9e7611725a011c72ddc
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-21 08:54:03 +0900

    abc286_a

--- abc286/src/bin/a.rs
+++ abc286/src/bin/a.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        n: usize, ((p, q), (r, s)): ((Usize1, Usize1), (Usize1, Usize1)),
+        a: [i32; n],
     }
-    todo!();
+    println!(
+        "{}",
+        (0..n)
+            .map(|i| match i {
+                x if (p..=q).contains(&x) => a[r + x - p],
+                x if (r..=s).contains(&x) => a[p + x - r],
+                _ => a[i],
+            })
+            .join(" ")
+    );
 }
