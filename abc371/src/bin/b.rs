a829fee0c41c29db6e61c5f3d87c6d08db92fea5
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-22 21:16:01 +0900

    abc371_b

--- abc371/src/bin/b.rs
+++ abc371/src/bin/b.rs
@@ -1,8 +1,24 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        _n: usize, m: usize,
+        ab: [(Usize1, char); m],
     }
-    todo!();
+    println!(
+        "{}",
+        ab.iter()
+            .enumerate()
+            .map(|(i, (a, b))| *b == 'M' && ab.iter().take(i).all(|(pa, pb)| pa != a || *pb == 'F'))
+            .map(yes_no)
+            .join("\n")
+    );
+}
+
+fn yes_no(b: bool) -> String {
+    match b {
+        true => "Yes".to_string(),
+        false => "No".to_string(),
+    }
 }
