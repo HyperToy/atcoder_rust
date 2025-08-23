a5f6bef2f1265c69283d0219e3d7e31411cee044
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-23 09:56:47 +0900

    abc395_d

--- abc395/src/bin/d.rs
+++ abc395/src/bin/d.rs
@@ -2,34 +2,32 @@ use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n: usize, q: usize,
+        n: usize, q: u32,
     }
-    let mut pos = (0..n).collect::<Vec<_>>();
-    // 箱i は今は s[i] と呼ばれている
-    let mut s = (0..n).collect::<Vec<_>>();
-    let mut si = (0..n).collect::<Vec<_>>();
+    let mut f = (0..n).collect::<Vec<_>>(); // 鳩 -> 巣
+    let mut g = (0..n).collect::<Vec<_>>(); // 巣 -> ラベル
+    let mut h = (0..n).collect::<Vec<_>>(); // ラベル -> 鳩
     for _ in 0..q {
-        input! { t: i32 }
+        input! { t: u8 }
         match t {
             1 => {
                 input! { a: Usize1, b: Usize1 }
-                pos[a] = si[b];
+                f[a] = h[b];
             }
             2 => {
                 input! { a: Usize1, b: Usize1 }
-                swap(&mut s, a, b);
-                swap(&mut si, a, b);
+                let p = h[a]; // ラベルaの巣
+                let q = h[b]; // ラベルbの巣
+                h[a] = q;
+                h[b] = p;
+                g[p] = b;
+                g[q] = a;
             }
             3 => {
                 input! { a: Usize1 }
-                println!("{}", s[pos[a]] + 1);
+                println!("{}", g[f[a]] + 1);
             }
             _ => unreachable!(),
         }
     }
-}
-fn swap(v: &mut Vec<usize>, a: usize, b: usize) {
-    let tmp = v[a];
-    v[a] = b;
-    v[b] = tmp;
 }
