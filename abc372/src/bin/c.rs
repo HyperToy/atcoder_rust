f861ccef5eb8e2b8244a9dcebea095b1c527692d
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-25 08:45:41 +0900

    abc372_c

--- abc372/src/bin/c.rs
+++ abc372/src/bin/c.rs
@@ -7,25 +7,24 @@ fn main() {
         s: Chars,
         qs: [(Usize1, char); q],
     }
-    let count = judge_abc_count(&s);
-    let answer = qs
-        .iter()
-        .fold((s, count, vec![]), |(s, count, mut answer), (x, c)| {
-            let (s, count) = process_query(&s, (*x, *c), count);
-            answer.push(count);
-            (s, count, answer)
-        });
-    println!("{}", answer.2.iter().join("\n"));
+    let mut count = judge_abc_count(&s);
+    let mut s = s;
+    let mut answer = vec![];
+    for (x, c) in qs {
+        count = process_query(&mut s, (x, c), count);
+        answer.push(count);
+    }
+    println!("{}", answer.iter().join("\n"));
 }
 
-fn process_query(s: &[char], query: (usize, char), count: usize) -> (Vec<char>, usize) {
+fn process_query(s: &mut [char], query: (usize, char), count: usize) -> usize {
     let (x, c) = query;
     let min = if x < 2 { 0 } else { x - 2 };
     let max = if x + 3 > s.len() { s.len() } else { x + 3 };
     let count = count - judge_abc_count(&s[min..max]);
-    let s = [&s[0..x], &[c], &s[x + 1..]].concat();
+    s[x] = c;
     let count = count + judge_abc_count(&s[min..max]);
-    (s, count)
+    count
 }
 
 fn judge_abc_count(s: &[char]) -> usize {
