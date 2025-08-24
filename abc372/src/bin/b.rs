083e00aa88bbdba6f37fed88bcbffad14ae12de6
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-24 09:33:51 +0900

    refactor: abc372_b

--- abc372/src/bin/b.rs
+++ abc372/src/bin/b.rs
@@ -5,7 +5,12 @@ fn main() {
     input! {
         m: u32,
     }
-    let answer = (0..=10)
+    let answer = solve(m);
+    println!("{}\n{}", answer.len(), answer.iter().join(" "));
+}
+
+fn solve(m: u32) -> Vec<u32> {
+    (0..=10)
         .rev()
         .fold((vec![], m), |(mut v, mut m), i| {
             let p = 3_u32.pow(i);
@@ -15,6 +20,20 @@ fn main() {
             }
             (v, m)
         })
-        .0;
-    println!("{}\n{}", answer.len(), answer.iter().join(" "));
+        .0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve() {
+        assert_eq!(solve(6), vec![1, 1]);
+        assert_eq!(solve(100), vec![4, 2, 2, 0]);
+        assert_eq!(
+            solve(59048),
+            vec![9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0],
+        );
+    }
 }
