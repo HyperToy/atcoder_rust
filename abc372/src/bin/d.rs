374c6c30f0bc2ee6719fe168d36e1687298848b6
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-26 08:18:08 +0900

    abc372_d

--- abc372/src/bin/d.rs
+++ abc372/src/bin/d.rs
@@ -1,8 +1,19 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        h: [u32; n],
     }
-    todo!();
+    let mut stack = vec![h[n - 1]];
+    let mut answer = vec![0];
+    for i in (0..n - 1).rev() {
+        answer.push(stack.len());
+        while stack.last().is_some_and(|&x| x < h[i]) {
+            stack.pop();
+        }
+        stack.push(h[i]);
+    }
+    println!("{}", answer.iter().rev().join(" "));
 }
