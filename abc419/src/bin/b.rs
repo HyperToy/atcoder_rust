004c673fc0c6644578b4e2733d36df54c6c315a5
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-30 08:44:02 +0900

    abc419_b

--- abc419/src/bin/b.rs
+++ abc419/src/bin/b.rs
@@ -1,8 +1,41 @@
+use itertools::Itertools;
 use proconio::input;
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 fn main() {
     input! {
-        //
+        q: usize,
     }
-    todo!();
+    let qs: Vec<Option<usize>> = (0..q)
+        .map(|_| {
+            input! {
+                t: usize,
+            }
+            match t {
+                1 => {
+                    input! {
+                        x: usize,
+                    }
+                    Some(x)
+                }
+                2 => None,
+                _ => unreachable!(),
+            }
+        })
+        .collect();
+    let mut bag = BinaryHeap::new();
+    let mut answer = vec![];
+    for q in qs {
+        match q {
+            Some(x) => {
+                bag.push(Reverse(x));
+            }
+            None => {
+                if let Some(Reverse(x)) = bag.pop() {
+                    answer.push(x);
+                }
+            }
+        }
+    }
+    println!("{}", answer.iter().join("\n"));
 }
