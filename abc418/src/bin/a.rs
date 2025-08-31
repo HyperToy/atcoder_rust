cdb6c30649375c0ef1456febc171e7adc311ebbe
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-08-31 21:21:32 +0900

    abc418_a

--- abc418/src/bin/a.rs
+++ abc418/src/bin/a.rs
@@ -1,8 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        _n: usize, s: Chars,
     }
-    todo!();
+    println!(
+        "{}",
+        yes_no(s.len() >= 3 && s[s.len() - 3..] == ['t', 'e', 'a'])
+    );
+}
+
+fn yes_no(b: bool) -> String {
+    if b { "Yes" } else { "No" }.to_string()
 }
