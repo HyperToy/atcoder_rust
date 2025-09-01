a9c36fbda04ccb32f3690cbd2ac8f8bc5ea6668e
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-01 09:20:04 +0900

    abc418_b

--- abc418/src/bin/b.rs
+++ abc418/src/bin/b.rs
@@ -1,8 +1,31 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        s: Chars,
     }
-    todo!();
+    let n = s.len();
+    let max_rate = (0..=n)
+        .cartesian_product(0..=n)
+        .filter(|(i, j)| i < j)
+        .map(|(i, j)| filling_rate(&s[i..j]))
+        // (a0 / a1) cmp (b0 / b1)
+        .sorted_by(|&a, &b| (a.0 * b.1).cmp(&(b.0 * a.1)))
+        .rev()
+        .next()
+        .map(|(x, t)| x as f64 / t as f64)
+        .unwrap();
+    println!("{:.9}", max_rate);
+}
+
+fn filling_rate(s: &[char]) -> (usize, usize) {
+    let n = s.len();
+    if n < 3 {
+        return (0, 1);
+    }
+    if (s[0], s[n - 1]) != ('t', 't') {
+        return (0, 1);
+    }
+    (s.iter().filter(|c| **c == 't').count() - 2, n - 2)
 }
