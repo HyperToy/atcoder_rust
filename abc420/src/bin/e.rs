c54d6f252482914a81e1db3e3b3f68e720ef1d94
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-04 09:07:41 +0900

    abc420_e

--- abc420/src/bin/e.rs
+++ abc420/src/bin/e.rs
@@ -1,8 +1,56 @@
-use proconio::input;
+use ac_library::Dsu;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        n: usize, q: usize,
     }
-    todo!();
+    let qs = (0..q).map(|_| {
+        input! {
+            t: u32,
+        }
+        match t {
+            1 => {
+                input! {
+                    u: Usize1, v: Usize1,
+                }
+                (t, u, v)
+            }
+            2 | 3 => {
+                input! {
+                    v: Usize1,
+                }
+                (t, v, 0)
+            }
+            _ => unreachable!(),
+        }
+    });
+
+    let mut dsu = Dsu::new(n);
+    let mut black = vec![false; n];
+    let mut count = vec![0; n];
+    let answer = qs.filter_map(|q| match q {
+        (1, u, v) => {
+            if !dsu.same(u, v) {
+                let c = count[dsu.leader(u)] + count[dsu.leader(v)];
+                dsu.merge(u, v);
+                count[dsu.leader(u)] = c;
+            }
+            None
+        }
+        (2, v, _) => {
+            count[dsu.leader(v)] += if black[v] { -1 } else { 1 };
+            black[v] ^= true;
+            None
+        }
+        (3, v, _) => Some(count[dsu.leader(v)] > 0),
+        _ => unreachable!(),
+    });
+
+    println!("{}", answer.map(yes_no).join("\n"))
+}
+
+fn yes_no(b: bool) -> String {
+    if b { "Yes" } else { "No" }.to_string()
 }
