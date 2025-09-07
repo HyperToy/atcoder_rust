44949041e9d69072bd123eb399a79e896fa6a263
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-08 08:33:15 +0900

    abc416_a

--- abc416/src/bin/a.rs
+++ abc416/src/bin/a.rs
@@ -1,8 +1,20 @@
-use proconio::input;
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
     input! {
-        //
+        _n: usize, l: Usize1, r: Usize1,
+        s: Chars,
     }
-    todo!();
+    println!("{}", yes_no(s[l..=r].iter().all(|c| c == &'o')));
+}
+
+fn yes_no(b: bool) -> &'static str {
+    if b {
+        "Yes"
+    } else {
+        "No"
+    }
 }
