7c7df7432153cf1550a0e904ffcfe67cc36dc075
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-09 08:11:47 +0900

    abc417_b

--- abc417/src/bin/b.rs
+++ abc417/src/bin/b.rs
@@ -1,8 +1,25 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, m: usize,
+        a: [i32; n],
+        b: [i32; m],
     }
-    todo!();
+    let mut answer = vec![];
+    let mut s = b.into_iter().sorted().rev().collect::<Vec<_>>();
+    for &a in &a {
+        while s.last().is_some_and(|&x| x < a) {
+            s.pop();
+        }
+        if s.last() == Some(&a) {
+            s.pop();
+        } else {
+            answer.push(a);
+        }
+    }
+    if !answer.is_empty() {
+        println!("{}", answer.into_iter().join(" "));
+    }
 }
