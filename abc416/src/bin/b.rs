f6afc62f9d40afe19c8a663a1780208b8077fbc0
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-10 08:12:38 +0900

    abc416_b

--- abc416/src/bin/b.rs
+++ abc416/src/bin/b.rs
@@ -1,8 +1,21 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        s: Chars
     }
-    todo!();
+    let n = s.len();
+    let t = (0..n)
+        .map(|i| {
+            if s[i] == '#' {
+                '#'
+            } else if i == n - 1 || s[i] == '.' && s[i + 1] == '#' {
+                'o'
+            } else {
+                '.'
+            }
+        })
+        .join("");
+    println!("{}", t);
 }
