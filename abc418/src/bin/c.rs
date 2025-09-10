80d211f2e346fe1d23bd1bed29a6147fba7ae4f2
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-11 08:23:05 +0900

    abc418_c

--- abc418/src/bin/c.rs
+++ abc418/src/bin/c.rs
@@ -1,8 +1,34 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, q: usize,
+        a: [usize; n],
+        qs: [usize; q],
     }
-    todo!();
+    let (s, max) = f(a);
+    let answer = qs
+        .into_iter()
+        .map(|q| if q > max { -1 } else { s[q] })
+        .join("\n");
+    println!("{}", answer);
+}
+
+fn f(a: Vec<usize>) -> (Vec<i64>, usize) {
+    let max = *a.iter().max().unwrap();
+    let mut s = vec![0; max + 1];
+    for a in a {
+        s[a] += 1;
+    }
+    for i in (1..max).rev() {
+        s[i] += s[i + 1];
+    }
+    for i in 0..max {
+        s[i + 1] += s[i];
+    }
+    for i in (0..max).rev() {
+        s[i + 1] = s[i] + 1;
+    }
+    (s, max)
 }
