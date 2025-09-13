994dbddc84bbede33517639b21f93196da572d5f
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-14 07:39:31 +0900

    abc414_a

--- abc414/src/bin/a.rs
+++ abc414/src/bin/a.rs
@@ -2,7 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, l: i32, r: i32,
+        ranges: [(i32, i32); n],
     }
-    todo!();
+    println!(
+        "{}",
+        ranges
+            .into_iter()
+            .filter(|(x, y)| *x <= l && *y >= r)
+            .count()
+    );
 }
