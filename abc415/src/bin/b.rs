c26aa51fbfa6749b054291a5feba790e4d8e5a39
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-13 18:36:11 +0900

    abc415_b

--- abc415/src/bin/b.rs
+++ abc415/src/bin/b.rs
@@ -1,8 +1,19 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        //
+        s: Chars
     }
-    todo!();
+    println!(
+        "{}",
+        s.into_iter()
+            .enumerate()
+            .filter(|(_, c)| *c == '#')
+            .collect::<Vec<_>>()
+            .chunks_exact(2)
+            .into_iter()
+            .map(|chunk| format!("{},{}", chunk[0].0 + 1, chunk[1].0 + 1))
+            .join("\n")
+    );
 }
