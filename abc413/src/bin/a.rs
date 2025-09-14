5258117b0caa277c49154bc8e34218fb41b875b9
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-15 07:11:31 +0900

    abc413_a

--- abc413/src/bin/a.rs
+++ abc413/src/bin/a.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize, m: i32,
+        a: [i32; n],
     }
-    todo!();
+    println!("{}", yes_no(a.into_iter().sum::<i32>() <= m));
+}
+
+fn yes_no(b: bool) -> &'static str {
+    if b {
+        "Yes"
+    } else {
+        "No"
+    }
 }
