f9aeb388b35e943fe4ebfcf5e6f93b9ed6c47b20
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-16 09:17:33 +0900

    abc414_b

--- abc414/src/bin/b.rs
+++ abc414/src/bin/b.rs
@@ -1,8 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [(char, usize); n],
     }
-    todo!();
+    println!("{}", solve(&a));
+}
+
+fn solve(a: &Vec<(char, usize)>) -> String {
+    if a.iter().any(|(_, l)| *l > 100) {
+        return "Too Long".to_string();
+    }
+    if a.iter().map(|(_, l)| l).sum::<usize>() > 100 {
+        return "Too Long".to_string();
+    }
+    a.iter()
+        .map(|(c, l)| std::iter::repeat(*c).take(*l).collect::<String>())
+        .join("")
 }
