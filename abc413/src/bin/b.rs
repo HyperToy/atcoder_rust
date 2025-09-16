6d6367396fa34f5406b0332735eb68a51ef6273b
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-17 07:53:17 +0900

    abc413_b

--- abc413/src/bin/b.rs
+++ abc413/src/bin/b.rs
@@ -1,8 +1,20 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        s: [String; n],
     }
-    todo!();
+    println!(
+        "{}",
+        s.iter()
+            .enumerate()
+            .cartesian_product(s.iter().enumerate())
+            .filter(|((i, _), (j, _))| i != j)
+            .map(|((_, s), (_, t))| format!("{}{}", s, t))
+            .sorted()
+            .dedup()
+            .count()
+    );
 }
