b58a1c2ba1ad35dcc28e08ea480fcc2abc33968d
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-16 10:09:42 +0900

    refactor: abc418_c

--- abc418/src/bin/c.rs
+++ abc418/src/bin/c.rs
@@ -10,25 +10,31 @@ fn main() {
     let (s, max) = f(a);
     let answer = qs
         .into_iter()
-        .map(|q| if q > max { -1 } else { s[q] })
+        .map(|q| if q > max { None } else { s[q] })
+        .map(|x| x.map(|x| x.to_string()).unwrap_or("-1".to_string()))
         .join("\n");
     println!("{}", answer);
 }
 
-fn f(a: Vec<usize>) -> (Vec<i64>, usize) {
+fn f(a: Vec<usize>) -> (Vec<Option<usize>>, usize) {
+    let n = a.len();
     let max = *a.iter().max().unwrap();
-    let mut s = vec![0; max + 1];
+    let mut sum = vec![0; max + 1];
+    let mut count = vec![0; max + 1];
     for a in a {
-        s[a] += 1;
+        sum[a] += a;
+        count[a] += 1;
     }
-    for i in (1..max).rev() {
-        s[i] += s[i + 1];
+    for i in 1..=max {
+        sum[i] += sum[i - 1];
+        count[i] += count[i - 1];
     }
-    for i in 0..max {
-        s[i + 1] += s[i];
-    }
-    for i in (0..max).rev() {
-        s[i + 1] = s[i] + 1;
-    }
-    (s, max)
+    let answer = std::iter::once(None)
+        .chain(
+            (1..=max)
+                .map(|b| sum[b - 1] + (n - count[b - 1]) * (b - 1) + 1)
+                .map(|x| Some(x)),
+        )
+        .collect_vec();
+    (answer, max)
 }
