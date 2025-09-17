b34f58966f3fc232099b3631d4b3435f956b0a47
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-18 08:33:16 +0900

    abc417_c

--- abc417/src/bin/c.rs
+++ abc417/src/bin/c.rs
@@ -2,7 +2,17 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        n: usize,
+        a: [usize; n],
     }
-    todo!();
+    let max = *a.iter().max().unwrap();
+    let mut count = vec![(0, 0); max * 2 + n];
+    a.iter()
+        .enumerate()
+        .map(|(i, x)| (max + i + *x, max + i - *x))
+        .for_each(|(x, y)| {
+            count[x].0 += 1;
+            count[y].1 += 1
+        });
+    println!("{}", count.iter().map(|(x, y)| x * y).sum::<usize>());
 }
