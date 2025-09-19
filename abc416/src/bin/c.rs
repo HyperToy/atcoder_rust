7b03e17443bd4df8c32e3b2df240cdb8a84bd18f
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-19 09:53:20 +0900

    abc416_c

--- abc416/src/bin/c.rs
+++ abc416/src/bin/c.rs
@@ -1,8 +1,17 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        //
+        n: usize, k: usize, x: Usize1,
+        s: [String; n],
     }
-    todo!();
+    let answer = (0..k)
+        .map(|_| 0..n)
+        .multi_cartesian_product()
+        .map(|p| p.iter().map(|i| &s[*i]).join(""))
+        .sorted()
+        .nth(x)
+        .unwrap();
+    println!("{}", answer);
 }
