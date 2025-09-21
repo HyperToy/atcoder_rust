2a2e489d4d32cb5283ff5c5e1bf4cc9be8dd2b4d
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-21 09:38:28 +0900

    abc411_a

--- abc411/src/bin/a.rs
+++ abc411/src/bin/a.rs
@@ -2,7 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-        //
+        p: String,
+        l: usize,
     }
-    todo!();
+    println!("{}", yes_no(p.len() >= l));
+}
+
+fn yes_no(b: bool) -> &'static str {
+    if b {
+        "Yes"
+    } else {
+        "No"
+    }
 }
