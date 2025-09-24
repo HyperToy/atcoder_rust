2949c4386622683c431036016cf853cc6743d9c5
NODA Ryohei <ryohey.trumpet@gmail.com>
2025-09-24 09:27:33 +0900

    refactor: abc418_d

--- abc418/src/bin/d.rs
+++ abc418/src/bin/d.rs
@@ -5,22 +5,12 @@ fn main() {
         n: usize,
         t: Chars,
     }
-    let t = t
-        .into_iter()
-        .map(|c| match c {
-            '1' => 1,
-            '0' => 0,
-            _ => unreachable!(),
-        })
-        .collect::<Vec<_>>();
-    let mut s = vec![0; n + 1];
-    for i in 0..n {
-        s[i + 1] = s[i] + if t[i] == 0 { 1 } else { 0 };
-    }
     let mut answer = 0i64;
-    let (mut even, mut odd) = (1i64, 0i64);
-    for i in 1..=n {
-        if s[i] % 2 == 0 {
+    let (mut even, mut odd) = (1, 0);
+    let mut sum = 0;
+    for i in 0..n {
+        sum += if t[i] == '0' { 1 } else { 0 };
+        if sum % 2 == 0 {
             answer += even;
             even += 1;
         } else {
